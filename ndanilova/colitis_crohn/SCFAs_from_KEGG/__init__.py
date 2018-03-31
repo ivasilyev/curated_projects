692ed5c1fd57a506a077c7f28bb6606ab1906650
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2018-04-01 00:28:49 +0300

    Added 'SCFAs_from_KEGG' subtask script

---
+++ ndanilova/colitis_crohn/SCFAs_from_KEGG/__init__.py
@@ -0,0 +1,390 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+# Pre-setup:
+docker pull python:latest && \
+docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it python:latest bash
+
+docker pull ivasilyev/env_25_ecoli_genes:latest && \
+docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it ivasilyev/env_25_ecoli_genes:latest bash
+pip3 install pandas xlrd requests bs4 lxml jinja2 pyyaml
+python3
+
+"""
+
+import os
+import re
+import pandas as pd
+import xlrd  # Required by pandas to parse Excel sheets
+import requests
+import bs4
+import lxml  # Required by bs4
+import multiprocessing
+from collections import Counter
+import subprocess
+import yaml
+
+
+def ends_with_slash(string):
+    if string.endswith("/"):
+        return string
+    else:
+        return str(string + "/")
+
+
+def is_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError:
+        pass
+
+
+# Manage output location
+outputDir = "/data1/bio/projects/ndanilova/"
+is_path_exists(outputDir)
+# Move the Excel DB table into outputDir and parse it
+
+
+def parse_raw_database(xlsx):
+    # Well, that was a something. Hope we'll get better data next time.
+    col_names_list = ["sample_name", "group_id"]
+    xl = pd.ExcelFile(xlsx)
+    df = pd.DataFrame(columns=col_names_list)
+    def append_group_dataframe(dataframe, group_list, group_id):
+        return dataframe.append(pd.DataFrame({"sample_name": [re.sub('^0+', "", re.findall('(\d+)', re.sub('[ \r\n]', "", i))[0]) + re.findall('([A-Za-z]+)', re.sub('[ \r\n]', "", i))[0] for i in group_list], "group_id": [group_id,] * len(group_list)}), ignore_index=True)
+    for sheet_id, col_name, group_id in zip(["ремиссия ЯК", "обострение ЯК", "обострение БК", "ремиссия БК"], ["# образца/Sample #", "# образца/Sample #", "# образца", "# образца"], ["colitis_rem", "colitis_esc", "crohn_esc", "crohn_rem"]):
+        df1 = xl.parse(sheet_id)
+        try:
+            df = append_group_dataframe(df, df1[col_name].values.tolist(), group_id)
+        except KeyError:
+            print([sheet_id, col_name, group_id], list(df1))
+            raise
+    return df.loc[:, col_names_list]
+
+
+# Parse base groups
+groupDataDF = parse_raw_database(outputDir + "1 База на статистику самый точный вариант от 14.05.2017 (1).xlsx")
+# And add the control group
+groupDataDF = groupDataDF.append(pd.read_table("/data2/bio/Metagenomes/SampleData/GROUPDATA_SRR.tsv", sep='\t', header='infer', names=list(groupDataDF), engine='python'))
+
+class SampleDataArray:
+    def __init__(self, dictionary):
+        self.as_dict = dict(dictionary)
+    @staticmethod
+    def load_sampledata(file):
+        try:
+            return SampleDataArray({j[0]: j[1:] for j in [i.split('\t') for i in SampleDataArray.file_to_list(file) if len(i) > 0]})
+        except IndexError:
+            raise ValueError("Cannot parse sample data: " + file)
+    @staticmethod
+    def file_to_list(file):
+        file_buffer = open(file, 'rU')
+        output_list = [j for j in [re.sub('[\r\n]', '', i) for i in file_buffer] if len(j) > 0]
+        file_buffer.close()
+        return output_list
+    def get(self, key):
+        return self.as_dict.get(key)
+    def add(self, dictionary):
+        self.as_dict.update(dictionary)
+    def export(self):
+        return "\n".join(["\t".join([i] + self.as_dict[i]) for i in self.as_dict]) + "\n"
+    @staticmethod
+    def var_to_file(var_to_write, file_to_write):
+        file = open(file_to_write, 'w')
+        file.write(var_to_write)
+        file.close()
+    def dump(self, file):
+        self.var_to_file(self.export(), file)
+
+
+# Export sample data
+wholeSampleDataArray = SampleDataArray.load_sampledata("/data2/bio/Metagenomes/SampleData/SAMPLEDATA_READS_SOLID_ILLUMINA_NO_HG19.txt")
+processedSampleDataArray = SampleDataArray({i: wholeSampleDataArray.get(i) for i in sorted(list(set(wholeSampleDataArray.as_dict).intersection(groupDataDF["sample_name"].values.tolist())))})
+sampleDataFileName = outputDir + "_".join(sorted(list(set(groupDataDF["group_id"].values.tolist())))) + ".sampledata"
+processedSampleDataArray.dump(sampleDataFileName)
+
+
+class FASTA:
+    """
+    This class is an attempt to apply NCBI standards to single FASTA.
+    Consumes one header followed by sequence.
+    """
+    def __init__(self, single_fasta):
+        self._body = re.sub("\n+", "\n", single_fasta.replace('\r', ''))
+        try:
+            self.header = re.findall(">(.+)", self._body)[0].strip()
+            self.sequence = "\n".join(self.chunk_string(re.sub("[^A-Za-z]", "", self._body.replace(self.header, "")), 70)).upper()
+        except IndexError:
+            raise ValueError("Cannot parse the header!")
+        # Nucleotide sequence has only AT(U)GC letters. However, it may be also protein FASTA.
+    @staticmethod
+    def chunk_string(string, length):
+        return [string[0 + i:length + i] for i in range(0, len(string), length)]
+    def __len__(self):
+        return len(self.sequence)
+    def to_dict(self):
+        return {self.header: self.sequence}
+    def to_str(self):
+        return "\n".join([">" + self.header, self.sequence])
+
+
+def remove_empty_values(input_list):
+    try:
+        return list(filter(lambda x: len(x) > 0, input_list))
+    except TypeError:
+        print(input_list)
+        return []
+
+
+def multi_core_queue(function_to_parallelize, queue):
+    pool = multiprocessing.Pool()
+    output = pool.map(function_to_parallelize, queue)
+    pool.close()
+    pool.join()
+    return output
+
+
+class KEGGCompoundSequencesRetriever:
+    """
+    This class processes KEGG web pages based on compound ID (CXXXXX)
+    """
+    def __init__(self, compound_id):
+        self.c_id = compound_id
+    @staticmethod
+    def load_page(url):
+        return bs4.BeautifulSoup(requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36 OPR/32.0.1948.25'}).content, "lxml")
+    def parse_compound_page(self):
+        """
+        The function processes URL like 'http://www.genome.jp/dbget-bin/www_bget?cpd:<Compound ID>'
+        :return:
+        Enzymes entries (X.X.X.X) list
+        """
+        _soup = self.load_page("http://www.genome.jp/dbget-bin/www_bget?cpd:" + self.c_id)
+        try:
+            table_soup = _soup.find_all("td", "fr2")[0]
+        except IndexError:
+            return ""  # Page may be invalid
+        for row_soup in table_soup.find_all("tr"):
+            try:
+                key_cell_soup = row_soup.find_all("th", "th20")[0]
+                if key_cell_soup.text == "Enzyme":
+                    enzyme_soup = row_soup.find_all("td", "td20")[0]
+                    return [i.text for i in enzyme_soup.find_all("a")]
+            except IndexError:
+                continue
+    def parse_enzyme_page(self, e_id):
+        """
+        The function processes URL like 'http://www.genome.jp/dbget-bin/www_bget?ec:<Enzyme ID>'
+        :param e_id: str
+        Enzyme ID corresponding the template 'X.X.X.X'.
+        :return: list
+        Orthologs list
+        # Dictionary {'Orthology': [orthologs_list], 'Genes': [genes_list]}
+        """
+        _soup = self.load_page("http://www.genome.jp/dbget-bin/www_bget?ec:" + e_id)
+        try:
+            table_soup = _soup.find_all("td", "fr2")[0]
+        except IndexError:
+            return ""
+        out_dict = {"Orthology": [], "Genes": []}
+        for row_soup in table_soup.find_all("tr"):
+            try:
+                if row_soup.find_all("th", "th21")[0].text == "Orthology":
+                    orthologs_soup = row_soup.find_all("td", "td21")[0]
+                    out_dict["Orthology"].extend([i.text for i in orthologs_soup.find_all("a")])
+            except IndexError:
+                pass
+            try:
+                if row_soup.find_all("th", "th20")[0].text == "Genes":
+                    for genes_soup in row_soup.find_all("td", "td20")[0].find_all("table")[0].find_all("tr"):
+                        organism_id = genes_soup.find_all("nobr")[0].text.strip().lower()
+                        gene_id = genes_soup.find_all("a")[0].text.strip()
+                        out_dict["Genes"].append(organism_id + gene_id)
+            except IndexError:
+                pass
+        return out_dict["Orthology"]
+    def parse_ortholog_page(self, k_id):
+        """
+        The function processes URL like 'http://www.genome.jp/dbget-bin/www_bget?ko:<Orthology ID>'
+        :param k_id: str
+            Orthology ID corresponding the template 'KXXXXX'
+        :return: list
+        List [<Organism id>:<Gene ID>]
+        """
+        _soup = KEGGCompoundSequencesRetriever.load_page("http://www.genome.jp/dbget-bin/www_bget?ko:" + k_id)
+        try:
+            table_soup = _soup.find_all("td", "fr4")[0]
+        except IndexError:
+            return ""
+        genes_list = []
+        for row_soup in table_soup.find_all("tr"):
+            try:
+                if row_soup.find_all("th", "th40")[0].text == "Genes":
+                    for gene_soup in row_soup.find_all("td", "td40")[0].find_all("table")[0].find_all("tr"):
+                        organism_id = gene_soup.find_all("nobr")[0].text.strip().lower()
+                        gene_ids_list = [i.text.strip() for i in gene_soup.find_all("a")]
+                        genes_list.extend([organism_id + i for i in gene_ids_list])
+            except IndexError:
+                pass
+        return genes_list
+    def parse_gene_page(self, g_id):
+        """
+        The function processes URL like 'http://www.genome.jp/dbget-bin/www_bget?<Gene ID>'
+        :param g_id: Gene ID corresponding the template '<Organism ID>:<Entry ID>'
+        :return: Single FASTA object
+        """
+        _soup = self.load_page("http://www.genome.jp/dbget-bin/www_bget?" + g_id)
+        try:
+            table_soup = _soup.find_all("td", "fr1")[0]
+        except IndexError:
+            return ""
+        out_dict = {"g_id": g_id}
+        for row_soup in table_soup.find_all("tr"):
+            try:
+                if row_soup.find_all("th", "th11")[0].text.strip() == "Definition":
+                    out_dict["definition"] = row_soup.find_all("td", "td11")[0].text
+            except IndexError:
+                pass
+            try:
+                if row_soup.find_all("th", "th10")[0].text.strip() == "KO":
+                    out_dict["k_id"] = row_soup.find_all("td", "td10")[0].find_all("div")[0].find_all("div")[0].find_all("a")[0].text.strip()
+                    out_dict["enzyme_name"] = row_soup.find_all("td", "td10")[0].find_all("div")[0].find_all("div")[1].text.strip()
+            except IndexError:
+                pass
+            try:
+                if row_soup.find_all("th", "th11")[0].text.strip() == "Organism":
+                    out_dict["organism_name"] = " ".join([i.strip() for i in row_soup.find_all("td", "td11")[0].find_all("div")[0].text.split()[1:]])
+            except IndexError:
+                pass
+            try:
+                if row_soup.find_all("th", "th11")[0].text.strip() == "NT seq":
+                    out_dict["sequence"] = "".join([i.strip().upper() for i in row_soup.find_all("td", "td11")[0].text.split('\n')[1:]])
+            except IndexError:
+                pass
+        out_dict = {i: out_dict.get(i) if out_dict.get(i) else "" for i in ["g_id", "organism_name", "k_id", "enzyme_name", "definition", "sequence"]}
+        return FASTA(">" + " ".join([out_dict.get(i).strip() for i in ["g_id", "organism_name", "k_id", "enzyme_name", "definition"]]) + "\n" + out_dict["sequence"])
+    def compound2orthologs_list(self):
+        e_ids_list = remove_empty_values(self.parse_compound_page())
+        return sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_enzyme_page, e_ids_list) for k in j]))))
+    def compound2genes_list(self):
+        e_ids_list = remove_empty_values(self.parse_compound_page())
+        k_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_enzyme_page, e_ids_list) for k in j]))))
+        return sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_ortholog_page, k_ids_list) for k in j]))))
+    def compound2fasta(self):
+        e_ids_list = remove_empty_values(self.parse_compound_page())
+        k_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_enzyme_page, e_ids_list) for k in j]))))
+        g_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_ortholog_page, k_ids_list) for k in j]))))
+        return remove_empty_values(multi_core_queue(self.parse_gene_page, g_ids_list))
+    def genes_list2fasta(self, *args):
+        # Just like the previous, but the 'g_ids_list' is given separately
+        e_ids_list = remove_empty_values(self.parse_compound_page())
+        k_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_enzyme_page, e_ids_list) for k in j]))))
+        g_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(self.parse_ortholog_page, k_ids_list) for k in j]).intersection(set(args)))))
+        return remove_empty_values(multi_core_queue(self.parse_gene_page, g_ids_list))
+
+
+# Create retrieving objects
+scfaCompoundsIDsDict = {"formate": "C00058", "acetate": "C00033", "propionate": "C00163", "butyrate": "C00246", "isobutyrate": "C02632", "valerate": "C00803", "isovalerate": "C08262"}
+scfaJSON = {}
+foundGenesIDsList = []
+for scfaCompoundName in scfaCompoundsIDsDict:
+    scfaJSONDict = {"compound_name": scfaCompoundName, "compound_id": scfaCompoundsIDsDict[scfaCompoundName]}
+    scfaJSONDict["retriever"] = KEGGCompoundSequencesRetriever(scfaJSONDict["compound_id"])
+    scfaJSONDict.update({"orthologs_list": scfaJSONDict["retriever"].compound2orthologs_list(), "genes_list": scfaJSONDict["retriever"].compound2genes_list()})
+    foundGenesIDsList.extend(scfaJSONDict["genes_list"])
+    scfaJSON.update({scfaCompoundName: scfaJSONDict})
+
+# Compile all FASTA objects
+foundGenesIDsCounter = Counter(foundGenesIDsList)
+notOverlappingGenesIDsList = [i for i in foundGenesIDsCounter if foundGenesIDsCounter[i] == 1]
+notOverlappingGenesFASTAsList = [scfaJSON[i]["retriever"].genes_list2fasta(*notOverlappingGenesIDsList) for i in scfaJSON]
+
+# Export FASTA
+referenceDir = "/data/reference/custom/SCFAs_from_KEGG/"
+is_path_exists(referenceDir)
+SampleDataArray.var_to_file("\n".join([i.to_str() for i in remove_empty_values([k for j in notOverlappingGenesFASTAsList for k in j])]),
+                            referenceDir + "SCFAs_from_KEGG.fasta")
+
+
+
+# DEBUG
+testKegg = KEGGCompoundSequencesRetriever("C00246")
+fff = testKegg.compound2fasta()
+testKegg.parse_compound_page()
+testKegg.parse_enzyme_page("1.3.1.31")
+testKegg.parse_ortholog_page("K10797")
+testKegg.parse_gene_page("dda:Dd703_1770")
+e_ids_list = remove_empty_values(testKegg.parse_compound_page())
+k_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(testKegg.parse_enzyme_page, e_ids_list) for k in j]))))
+g_ids_list = sorted(remove_empty_values(list(set([k for j in multi_core_queue(testKegg.parse_ortholog_page, k_ids_list) for k in j]))))
+fasta_objs_list = remove_empty_values(multi_core_queue(testKegg.parse_gene_page, g_ids_list))
+[i.to_str() for i in fasta_objs_list]
+k_ids_list = sorted(list(set([k for j in remove_empty_values([remove_empty_values(testKegg.parse_enzyme_page(i)) for i in e_ids_list]) for k in j])))
+g_ids_list = sorted(list(set([k for j in remove_empty_values([remove_empty_values(testKegg.parse_ortholog_page(i)) for i in k_ids_list]) for k in j])))
+fasta_objs_list = [testKegg.parse_gene_page(i) for i in g_ids_list]
+filtered_fasta_objs_list = [i for i in fasta_objs_list if len(i.sequence) > 0]
+
+g_id ="dda:Dd703_1770"
+_soup = KEGGCompoundSequencesRetriever.load_page("http://www.genome.jp/dbget-bin/www_bget?" + g_id)
+table_soup = _soup.find_all("td", "fr1")[0]
+# for row_soup in table_soup.find_all("tr"):
+# g_id
+row_soup = table_soup.find_all("tr")[5]
+# if row_soup.find_all("th", "th11")[0].text.strip() == "Organism":
+organism_name = " ".join([i.strip() for i in row_soup.find_all("td", "td11")[0].find_all("div")[0].text.split()[1:]])
+# ko
+row_soup = table_soup.find_all("tr")[3]
+# if row_soup.find_all("th", "th10")[0].text.strip() == "KO":
+k_id = row_soup.find_all("td", "td10")[0].find_all("div")[0].find_all("div")[0].find_all("a")[0].text.strip()
+enzyme_name = row_soup.find_all("td", "td10")[0].find_all("div")[0].find_all("div")[1].text.strip()
+# def
+row_soup = table_soup.find_all("tr")[2]
+# if row_soup.find_all("th", "th11")[0].text.strip() == "Definition":
+definition = row_soup.find_all("td", "td11")[0].text
+# seq
+row_soup = table_soup.find_all("tr")[-1]
+# if row_soup.find_all("th", "th11")[0].text.strip() == "NT seq":
+sequence = ''.join([i.strip().upper() for i in row_soup.find_all("td", "td11")[0].text.split('\n')[1:]])
+
+k_id = "K10797"
+_soup = KEGGCompoundSequencesRetriever.load_page("http://www.genome.jp/dbget-bin/www_bget?ko:" + k_id)
+table_soup = _soup.find_all("td", "fr4")[0]
+genes_list = []
+# for row_soup in table_soup.find_all("tr"):
+row_soup = table_soup.find_all("tr")[9]
+# if row_soup.find_all("th", "th40")[0].text == "Genes":
+# for gene_soup in row_soup.find_all("td", "td40")[0].find_all("table")[0].find_all("tr")
+gene_soup = row_soup.find_all("td", "td40")[0].find_all("table")[0].find_all("tr")[51]
+organism_id = gene_soup.find_all("nobr")[0].text.strip().lower()
+gene_ids_list = [i.text.strip() for i in gene_soup.find_all("a")]
+genes_list.extend([organism_id + i for i in gene_ids_list])
+
+e_id = "1.3.1.31"
+_soup = KEGGCompoundSequencesRetriever.load_page("http://www.genome.jp/dbget-bin/www_bget?ec:" + e_id)
+table_soup = _soup.find_all("td", "fr2")[0]
+genes_list = []
+orthologs_list = []
+# for row_soup in table_soup.find_all("tr")
+row_soup = table_soup.find_all("tr")[16]
+# if row_soup.find_all("th", "th21")[0].text == "Orthology":
+orthologs_soup = row_soup.find_all("td", "td21")[0]
+orthologs_list.extend([i.text for i in orthologs_soup.find_all("a")])
+# if row_soup.find_all("th", "th20")[0].text == "Genes":
+# for genes_soup in row_soup.find_all("td", "td20")[0].find_all("table")[0].find_all("tr")
+genes_soup = row_soup.find_all("td", "td20")[0].find_all("table")[0].find_all("tr")[0]
+organism_id = genes_soup.find_all("nobr")[0].text.strip().lower()
+gene_id = genes_soup.find_all("a")[0].text.strip()
+genes_list.append(organism_id + ":" + gene_id)
+
+c = 0
+for row_soup in table_soup.find_all("tr"):
+    c += 1
+    try:
+        key_cell_soup = row_soup.find_all("th", "th40")[0]
+        if key_cell_soup.text == "Gene":
+            # print([i.text for i in row_soup.find_all("td", "td20")[0].find_all("a")])
+            print(c)
+            break
+    except IndexError:
+        continue
