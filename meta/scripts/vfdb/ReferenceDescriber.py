1004454559ab5da89658bda5550fca9ced4ab427
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2018-11-07 15:03:45 +0300

    Updated VFDB reference

--- meta/scripts/vfdb/ReferenceDescriber.py
+++ meta/scripts/vfdb/ReferenceDescriber.py
@@ -8,6 +8,10 @@ docker pull ${DOCKER_IMAGE_NAME} && \
 docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it ${DOCKER_IMAGE_NAME} python3
 """
 
+import os
+import subprocess
+from meta.templates.ReferenceDescriberTemplate import ReferenceDescriberTemplate
+
 """
 In the case of connection failure download data via chinese proxy from http://www.mgc.ac.cn/VFs/download.htm
 VFs description file
@@ -26,41 +30,31 @@ http://www.mgc.ac.cn/VFs/Down/VFDB_setB_pro.fas.gz
 
 
 class ReferenceDescriber:
+    alias = "vfdb_v2018.11.02"
     name = "VFDB"
     description = "A reference database for bacterial virulence factors"
     documentation = "http://www.mgc.ac.cn/VFs/main.htm"
-    # Change the following lines after reference update
-    alias = "vfdb_v2018.06.29"
-    refdata = "/data/reference/VFDB/vfdb_v2018.06.29/index/vfdb_v2018.06.29.json"
-
-    def export(self):
-        print("""
-Database alias: {a}
-REFDATA linker: {b}
-              """.format(a=self.alias, b=self.refdata))
-
-    def parse_refdata(self):
-        from meta.scripts.RefDataParser import RefDataParser
-        p = RefDataParser(self.refdata)
-        return p.get_parsed_list()
+    refdata = "/data/reference/VFDB/vfdb_v2018.11.02/index/vfdb_v2018.11.02_refdata.json"
 
 
 class SequenceRetriever:
     def __init__(self):
-        import os
-        describer = ReferenceDescriber()
-        self.alias = "{a}_v{b}".format(a=describer.name.lower(), b=self._get_last_friday())
-        self.reference_dir = "/data/reference/{a}/{b}/".format(a=describer.name, b=self.alias)
+        name = "VFDB"
+        self.alias = "{a}_v{b}".format(a=name.lower(), b=self._get_last_friday())
+        self.reference_dir = "/data/reference/{a}/{b}/".format(a=name, b=self.alias).replace(" ", "_")
         os.makedirs(self.reference_dir, exist_ok=True)
         self.raw_nfasta = "{a}{b}.fasta".format(a=self.reference_dir, b=self.alias)
         self.index_dir = "{}index/".format(self.reference_dir)
+        subprocess.getoutput("rm -rf {}".format(self.index_dir))
         self._download()
         self._get_index_guide()
         print("Please replace the following lines in control script:")
-        describer.alias = self.alias
-        describer.refdata = "{a}{b}.json".format(a=self.index_dir, b=self.alias)
+        # refdata = "{a}{b}.json".format(a=self.index_dir, b=self.alias)
+        describer = ReferenceDescriberTemplate(alias="{a}_v{b}".format(a=name.lower(), b=self._get_last_friday()),
+                                               name=name,
+                                               description="A reference database for bacterial virulence factors",
+                                               documentation="http://www.mgc.ac.cn/VFs/main.htm")
         describer.export()
-
     # VFDB updates at every Friday
     @staticmethod
     def _get_last_friday():
@@ -71,14 +65,15 @@ class SequenceRetriever:
         while last_friday.weekday() != calendar.FRIDAY:
             last_friday -= oneday
         return "{:%Y.%m.%d}".format(last_friday)
-
     def _download(self):
-        import subprocess
         url = "http://www.mgc.ac.cn/VFs/Down/VFDB_setB_nt.fas.gz"
         cmd = "curl -fsSL {a} | zcat > {b}".format(a=url, b=self.raw_nfasta)
         print(subprocess.getoutput(cmd))
         print("Got new raw reference: {}".format(self.raw_nfasta))
-
     def _get_index_guide(self):
         from meta.scripts.LaunchGuideLiner import LaunchGuideLiner
         LaunchGuideLiner.get_index_guide(index_directory=self.index_dir, raw_nfasta_file=self.raw_nfasta)
+
+
+if __name__ == '__main__':
+    sequenceRetriever = SequenceRetriever()
