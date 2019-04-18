f4862765eeb165c896ff1ea6fd4e9d434bb49861
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-04-18 14:06:52 +0300

    Refactored fields

--- meta/templates/ReferenceDescriberTemplate.py
+++ meta/templates/ReferenceDescriberTemplate.py
@@ -6,39 +6,58 @@ from abc import ABC, ABCMeta, abstractmethod
 
 class ReferenceDescriberTemplate(ABC):
     __metaclass__ = ABCMeta
-    alias = ""
-    name = ""
-    description = ""
-    documentation = ""
-    url = ""
-    refdata = ""
+    NAME = ""
+    VERSION = ""
+    ALIAS = ""
+    DESCRIPTION = ""
+    DOCUMENTATION = ""
+    WEBSITE = ""
+    REFDATA = ""
 
     def __init__(self):
         super().__init__()
         self._refdata_parser = ""
 
+    def update_alias(self):
+        self.ALIAS = "{}_v{}".format(self.NAME.lower(), self.VERSION.lower())
+        return self.ALIAS
+
     def export(self):
+        fields = """
+    NAME = ""
+    VERSION = ""
+    ALIAS = ""
+    DESCRIPTION = ""
+    DOCUMENTATION = ""
+    WEBSITE = ""
+    REFDATA = ""
+""".replace('""', '"{}"').format(self.NAME, self.VERSION, self.update_alias(), self.DESCRIPTION, self.DOCUMENTATION,
+                                 self.WEBSITE, self.REFDATA)
         print("""Please update the following script lines: 
 
-class ReferenceDescriber(ReferenceDescriberTemplate):
-    alias = "{ALIAS}"
-    name = "{NAME}"
-    description = "{DESCRIPTION}"
-    documentation = "{DOCUMENTATION}"
-    url = "{URL}"
-    refdata = "{REFDATA}"
+class ReferenceDescriber(ReferenceDescriberTemplate): 
+{}""".format(fields))
 
-""".format(ALIAS=self.alias, NAME=self.name, DESCRIPTION=self.description, DOCUMENTATION=self.documentation,
-           URL=self.url, REFDATA=self.refdata))
-
-    def get_index_guide(self, raw_nfasta_file):
+    @staticmethod
+    def get_index_guide(raw_nfasta_file):
         import os
         from meta.scripts.LaunchGuideLiner import LaunchGuideLiner
         LaunchGuideLiner.get_index_guide(
             index_directory=os.path.join(os.path.dirname(raw_nfasta_file), "index"),
             raw_nfasta_file=raw_nfasta_file)
 
+    @staticmethod
+    def find_refdata(index_dir):
+        import os
+        import subprocess
+        cmd = 'ls -d {}/* | grep "_refdata.json"'.format(os.path.normpath(index_dir))
+        out = subprocess.getoutput(cmd).strip()
+        if out.count("\n") > 0:
+            raise ValueError(
+                "Cannot find single reference data file! \nPlease check out the shell command: `{}`".format(cmd))
+        return out
+
     def get_refdata_dict(self):
         from meta.scripts.RefDataParser import RefDataParser
-        self._refdata_parser = RefDataParser(self.refdata)
+        self._refdata_parser = RefDataParser(self.REFDATA)
         return self._refdata_parser.refdata_lines_dict
