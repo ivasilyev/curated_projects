2129370059bd19f8da279882c4752aed0264d33b
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-05-03 12:10:11 +0300

    Added `main` class

--- meta/scripts/vfdb/ReferenceDescriber.py
+++ meta/scripts/vfdb/ReferenceDescriber.py
@@ -18,12 +18,12 @@ from meta.templates.ReferenceDescriberTemplate import ReferenceDescriberTemplate
 
 class ReferenceDescriber(ReferenceDescriberTemplate):
     NAME = "VFDB"
-    VERSION = "2018.11.09"
-    ALIAS = "vfdb_v2018.11.09"
+    VERSION = "2019.04.26"
+    ALIAS = "vfdb_v2019.04.26"
     DESCRIPTION = "A reference database for bacterial virulence factors"
     DOCUMENTATION = "https://www.ncbi.nlm.nih.gov/pubmed/30395255"
     WEBSITE = "http://www.mgc.ac.cn/VFs/main.htm"
-    REFDATA = "/data/reference/VFDB/vfdb_v2018.11.09/index/vfdb_v2018.11.09_refdata.json"
+    REFDATA = "/data/reference/VFDB/vfdb_v2019.04.26/index/vfdb_v2019.04.26_refdata.json"
 
 
 class SequenceRetriever:
@@ -60,13 +60,16 @@ class SequenceRetriever:
         out_file = Utilities.download_file(url=input_tuple[0], out_dir=input_tuple[1])
         Utilities.decompress_file(out_file)
     def retrieve(self):
-        print(Utilities.single_core_queue(self._download_handler, self._dl_queue))
+        tmp = Utilities.single_core_queue(self._download_handler, self._dl_queue)
+        del tmp
+        print("Download completed")
     def merge(self):
         Utilities.concatenate_files(*Utilities.scan_whole_dir(os.path.join(self.reference_dir, "nucleotide")),
                                     target_file=self.nfasta)
         Utilities.concatenate_files(*Utilities.scan_whole_dir(os.path.join(self.reference_dir, "protein")),
                                     target_file=self.pfasta)
         self.describer.get_index_guide(self.nfasta)
+        print("# Merge completed. \n# Protein FASTA to annotate: '{}'\n".format(self.pfasta))
 
 
 class Annotator:
@@ -75,8 +78,7 @@ class Annotator:
         self.annotation_file = self.describer.get_refdata_dict().get("sequence_1").annotation_file
         self._raw_pfasta_file = pfasta
         self._raw_nfasta_df = pd.DataFrame()
-
-        self._processed_nfasta_df, self.nfasta_df, self.pfasta_df, self.merged_df = (pd.DataFrame(),) * 4
+        self._processed_nfasta_df, self._processed_pfasta_df, self.pfasta_df, self.merged_df = (pd.DataFrame(),) * 4
     @staticmethod
     def _mp_parse_nfasta_header(header: str):
         _VFDB_REGEXES = (("vfdb_id", "^VFG(\d+)", "VFG{}"), ("gene_accession_id", "\(([^\(]+)\) ", "({}) "),
@@ -90,8 +92,9 @@ class Annotator:
             if len(out.get(key)) > 0:
                 header = header.replace(replacement.format(out.get(key)), "")
         return {k: out.get(k).strip() for k in out}
-    def _mp_parse_pfasta_header(self, header: str):
-        out = self._mp_parse_nfasta_header(header)
+    @staticmethod
+    def _mp_parse_pfasta_header(header: str):
+        out = Annotator._mp_parse_nfasta_header(header)
         out = {k.replace("gene", "protein"): out.get(k) for k in out}
         out["protein_header"] = out.pop("former_id")
         return out
@@ -114,8 +117,8 @@ class Annotator:
                                     Utilities.multi_core_queue(self._mp_parse_pfasta_header, raw_pfasta_headers)]
         self._processed_pfasta_df = Utilities.merge_pd_series_list(processed_pfasta_headers).sort_values("protein_header")
         self._processed_pfasta_df["vfdb_id"] = self._processed_pfasta_df["vfdb_id"].str.zfill(zf_len)
-        # Join provided table
-        vfs_table_file = os.path.join(os.path.dirname(os.path.dirname(self._raw_pfasta_file)), "VFs.xls")
+        # Join provided table. Note the table file placed into the same dir with the merged protein FASTA file
+        vfs_table_file = os.path.join(os.path.dirname(self._raw_pfasta_file), "VFs.xls")
         vfs_df = pd.read_excel(vfs_table_file, sheet_name="VFs", header=1).fillna("")
         vfs_df["vfdb_id"] = vfs_df["VFID"].str.extract("VF(\d+)")[0].str.zfill(zf_len)
         self.merged_df = pd.concat([i.set_index("vfdb_id").sort_index() for i in
@@ -124,9 +127,44 @@ class Annotator:
         self.merged_df.index.names = ["vfdb_id"]
         self.merged_df = self.merged_df.loc[self.merged_df["former_id"].str.len() > 0].reset_index()
         self.merged_df = Utilities.left_merge(self._raw_nfasta_df, self.merged_df, "former_id")
-        Utilities.dump_tsv(self.merged_df, "/data1/bio/projects/inicolaeva/klebsiella_infants/raw/test_ann.tsv")
     def export(self):
         import shutil
         shutil.copy2(self.annotation_file, "{}.bak".format(self.annotation_file))
         Utilities.dump_tsv(self.merged_df, table_file=self.annotation_file)
 
+
+if __name__ == '__main__':
+    retriever = SequenceRetriever()
+    retriever.retrieve()
+    retriever.merge()
+    """
+    # Reference indexing (from worker node):
+    
+    rm -rf /data/reference/VFDB/vfdb_v2019.04.26/index
+    export IMG=ivasilyev/bwt_filtering_pipeline_worker:latest && \
+    docker pull $IMG && \
+    docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 -it $IMG \
+    python3 /home/docker/scripts/cook_the_reference.py \
+    -i /data/reference/VFDB/vfdb_v2019.04.26/vfdb_v2019.04.26.fasta \
+    -o /data/reference/VFDB/vfdb_v2019.04.26/index
+    
+    # Wait until REFDATA file creates and complete the describer class template
+    
+    # Merge completed
+    # Protein FASTA to annotate: '/data/reference/VFDB/vfdb_v2019.04.26/vfdb_v2019.04.26_protein.fasta'
+    """
+    retriever.describer.set_refdata("/data/reference/VFDB/vfdb_v2019.04.26/index/vfdb_v2019.04.26_refdata.json")
+    """
+    Please update the following script lines:
+    class ReferenceDescriber(ReferenceDescriberTemplate):
+        NAME = "VFDB"
+        VERSION = "2019.04.26"
+        ALIAS = "vfdb_v2019.04.26"
+        DESCRIPTION = "A reference database for bacterial virulence factors"
+        DOCUMENTATION = "https://www.ncbi.nlm.nih.gov/pubmed/30395255"
+        WEBSITE = "http://www.mgc.ac.cn/VFs/main.htm"
+        REFDATA = "/data/reference/VFDB/vfdb_v2019.04.26/index/vfdb_v2019.04.26_refdata.json"
+    """
+    annotator = Annotator("/data/reference/VFDB/vfdb_v2019.04.26/vfdb_v2019.04.26_protein.fasta")
+    annotator.annotate()
+    annotator.export()
