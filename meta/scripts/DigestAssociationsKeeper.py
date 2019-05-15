adc1ea8651d8145aa0891b2a0a9dcdf92c3e44f0
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-05-15 15:22:19 +0300

    Transformed base digestion static method to instance method

--- meta/scripts/DigestAssociationsKeeper.py
+++ meta/scripts/DigestAssociationsKeeper.py
@@ -81,32 +81,66 @@ class DigestAssociationsKeeper:
         return arr[:n] + [["others", sum([i[1] for i in arr[n:]])]]
 
     @staticmethod
-    def digest_df(df: pd.DataFrame, associations: dict, columns_with_keywords: list, include_key: bool = True):
+    def prepare_string(s: str, lowercase: bool = False):
+        import re
+        out = re.sub(" +", " ", re.sub("[^A-Za-z0-9\-]+", " ", s)).strip()
+        if lowercase:
+            return out.lower()
+        return out
+
+    @staticmethod
+    def prepare_list(lst: list, lowercase: bool = False):
+        return Utilities.remove_empty_values([DigestAssociationsKeeper.prepare_string(i, lowercase) for i in lst])
+
+    def digest_df(self, df: pd.DataFrame, associations: dict, columns_with_keywords: list, include_key: bool = True,
+                  all_in_lowercase: bool = False, strict: bool = False):
+        """
+        :param df: Pandas DataFrame object containing an index, keyword columns and value columns
+        :param associations: Dictionary '{key: (keywords...)}'
+        :param columns_with_keywords: List of columns to search keywords
+        :param include_key: Should the key of keyword group be included?
+        :param all_in_lowercase: Convert both strings to lowercase?
+        :param strict: Only count full match
+        :return:
+        """
+        def __regular_search(s: str):
+            return any(i in str(s) for i in key_words)
+
+        def __strict_search(s: str):
+            return any(i == str(s) for i in key_words)
+
         df_columns = list(df)
-        columns_with_keywords = [i for i in columns_with_keywords if len(i) > 0]
+        columns_with_keywords = Utilities.remove_empty_values([i for i in columns_with_keywords])
+        columns_without_keywords = Utilities.remove_empty_values(
+            [i for i in df_columns if i not in columns_with_keywords])
         if len(columns_with_keywords) == 0:
             raise ValueError("No column for keyword search specified")
         try:
+            # 'columns_with_keywords' might be more than 1
             df["lookup_column"] = df.loc[:, columns_with_keywords].astype(str).apply(
-                lambda x: " ".join([CounterWrapper.prepare_string(i) for i in x]), axis=1)
+                lambda x: " ".join(self.prepare_list(x, lowercase=all_in_lowercase)), axis=1)
         except KeyError as e:
             print(e, list(df), associations, columns_with_keywords)
         keywords_series = []
+        out_dict = {}
         for main_word in associations:
             key_words = associations.get(main_word)
             if not key_words or len(key_words) == 0:
                 key_words = ()
             if include_key:
-                key_words = sorted(set([i.strip() for i in list(key_words) + [main_word, ]]))
-            else:
-                key_words = sorted([j for j in set([i.strip() for i in list(key_words)]) if len(j) > 0])
+                key_words = list(key_words) + [main_word, ]
+            key_words = sorted(set(self.prepare_list(key_words, lowercase=all_in_lowercase)))
             if len(key_words) == 0:
                 raise ValueError("No values to search: '{}: {}'".format(main_word, key_words))
-            sub_df = df.loc[df["lookup_column"].apply(lambda x: any(i.lower() in str(x) for i in key_words)) == True,
-                            [i for i in df_columns if i not in columns_with_keywords]]
-            keywords_series.append(sub_df.sum().rename(main_word))
-        out_df = pd.concat(keywords_series, axis=1, sort=False)
-        out_df.index.name = "sample_path"
-        out_df = out_df.transpose().fillna(0)
+            if strict:
+                df_with_keywords = df.loc[df["lookup_column"].apply(__strict_search) == True,
+                                          columns_without_keywords]
+            else:
+                df_with_keywords = df.loc[df["lookup_column"].apply(__regular_search) == True,
+                                          columns_without_keywords]
+            keywords_series.append(df_with_keywords.sum().rename(main_word))
+            out_dict[main_word] = df_with_keywords
+        out_df = Utilities.merge_pd_series_list(keywords_series).fillna(0)
+        out_df.columns.name = "sample_path"
         out_df.index.name = "keyword"
-        return out_df
+        return out_df, out_dict
