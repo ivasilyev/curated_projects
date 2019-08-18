e45a99bb26efb07468e22d363effb6fa2fbb163c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-08-18 06:12:12 +0300

    Added plasmid assembly filtering script

---
+++ meta/scripts/filter_plasmid_assembly_by_chromosome.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from meta.scripts.Utilities import Utilities
+from Bio import SeqIO
+
+
+class ArgParser:
+    def __init__(self):
+        from argparse import ArgumentParser
+        parser = ArgumentParser(description="Tool to remove from the plasmid nucleotide assembly the sequences which "
+                                            "are also presented in chromosome assembly",
+                                epilog="Sequences must be in FASTA format")
+        parser.add_argument("-c", "--chromosome", metavar="<chromosome.fna>", required=True,
+                            help="Input chromosome assembly file")
+        parser.add_argument("-p", "--plasmid", metavar="<plasmid.fna>", required=True,
+                            help="Input plasmid assembly file")
+        parser.add_argument("-r", "--rename", default=False, action="store_true",
+                            help="If checked, the output plasmid contigs will be renamed as 'contig<number>'")
+        parser.add_argument("-o", "--output", metavar="<output.fna>", required=True,
+                            help="Output plasmid assembly file")
+        self._namespace = parser.parse_args()
+        self.chromosome = self._namespace.chromosome
+        self.plasmid = self._namespace.plasmid
+        self.output = self._namespace.output
+
+
+class Filter:
+    def __init__(self, chromosome_file: str, plasmid_file: str, rename: bool = True):
+        chromosome_sequences = [i.seq for i in self._parse(chromosome_file)]
+        plasmid_records = self._parse(plasmid_file)
+        self.processed_records = []
+        for plasmid_record in plasmid_records:
+            if plasmid_record.seq not in chromosome_sequences:
+                self.processed_records.append(plasmid_record)
+        self.processed_records = Utilities.remove_duplicate_sequences(self.processed_records)
+        if rename:
+            for idx, seq_record_processed in enumerate(self.processed_records):
+                seq_record_processed.id = "contig{}".format(str(idx + 1).zfill(len(str(len(self.processed_records)))))
+                seq_record_processed.description = ""
+
+    @staticmethod
+    def _parse(assembly_file):
+        return sorted(list(SeqIO.parse(assembly_file, "fasta")), key=lambda x: len(x), reverse=True)
+
+    def export(self, out_file):
+        import os
+        os.makedirs(os.path.dirname(out_file), exist_ok=True)
+        SeqIO.write(self.processed_records, out_file, "fasta")
+
+
+if __name__ == '__main__':
+    argparser = ArgParser()
+    filter_ = Filter(argparser.chromosome, argparser.plasmid)
+    filter_.export(argparser.output)
