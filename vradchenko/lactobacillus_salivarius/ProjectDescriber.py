66ba01887986d47462bb39889b0f1d903d4462df
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2019-10-19 01:35:01 +0300

    Added description

---
+++ vradchenko/lactobacillus_salivarius/ProjectDescriber.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+from meta.templates.ProjectDescriberTemplate import ProjectDescriberTemplate
+
+
+class ProjectDescriber(ProjectDescriberTemplate):
+    OWNER = "vradchenko"
+    NAME = "lactobacillus_salivarius"
+    ROOT_DIR = os.path.join("/data1/bio/projects", OWNER, NAME)
+    RAW_DATA_DIR = os.path.join(ROOT_DIR, "raw")
+    MAPPED_DATA_DIR = os.path.join(ROOT_DIR, "mapped")
+    DATA_DIGEST_DIR = os.path.join(ROOT_DIR, "digest")
+    SAMPLE_DATA_DIR = os.path.join(ROOT_DIR, "sample_data")
+    SAMPLE_DATA_FILE = os.path.join(SAMPLE_DATA_DIR, "raw.sampledata")
+    GROUP_DATA_FILE = ""
