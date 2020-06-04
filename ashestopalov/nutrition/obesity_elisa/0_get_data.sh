97bef02baa8509cfe56c9345e04a7c335be5f3b7
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2020-06-04 19:47:57 +0300

    Replaced SHA1 generation with transliteration

--- ashestopalov/nutrition/obesity_elisa/0_get_data.sh
+++ ashestopalov/nutrition/obesity_elisa/0_get_data.sh
@@ -5,9 +5,11 @@ DIRNAME="/data1/bio/projects/ashestopalov/nutrition/obesity_elisa/raw"
 mkdir -p ${DIRNAME}
 cd ${DIRNAME}
 
-for FILE in "${DIRNAME}"/*
+
+for SRC in "${DIRNAME}"/*
   do
-    HASH=$(openssl sha1 "${FILE}" | grep -o '[^ ]*$')
-    EXTENSION=${FILE##*.}
-    mv "${FILE}" "${DIRNAME}/${HASH}.${EXTENSION}"
+    TGT=$(echo "${SRC}" | iconv -f utf8 -t koi8-r | catdoc -d us-ascii -s koi8-r)
+    echo "Rename: '${SRC}' -> '${TGT}'"
+    printf "\n"
+    mv "${SRC}" "${TGT}"
   done
