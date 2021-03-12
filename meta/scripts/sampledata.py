64afb210c74211f38239ee932c499ee2d71abed1
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-03-12 23:34:05 +0300

    Added validation

--- meta/scripts/sampledata.py
+++ meta/scripts/sampledata.py
@@ -6,15 +6,26 @@ from meta.scripts.Utilities import Utilities
 
 
 class SampleDataLine:
+    is_valid = False
+
     def __init__(self, sample_name: str, sample_read_files: list):
         self.name = sample_name.strip()
         self.reads = Utilities.remove_empty_values(sample_read_files)
+        self._validate_reads()
 
     def __eq__(self, other):
         return self.name == other.name
 
     def __lt__(self, other):
         return self.name < other.name
+
+    def _validate_reads(self):
+        c = 0
+        for read_file in self.reads:
+            if not os.path.isfile(read_file):
+                print("Not found the raw read file: '{}'".format(read_file))
+                c += 1
+        self.is_valid = c == 0
 
     def export(self):
         return "\t".join([self.name, ";".join(self.reads)])
@@ -27,7 +38,13 @@ class SampleDataArray:
         return len(self.lines)
 
     def validate(self):
-        self.lines = sorted([i for i in self.lines if i.exists])
+        o = []
+        for line in self.lines:
+            if line.is_valid():
+                o.append(line)
+            else:
+                print("Some raw read files are missing!")
+        self.lines = sorted(o)
 
     @staticmethod
     def generate(pair_2d_array: list, regex: str = "(.+)_S[0-9]+_L[0-9]+_R[0-9]+_[0-9]+"):
@@ -42,6 +59,6 @@ class SampleDataArray:
     def export(self):
         return "\n".join(["sample_name\traw_reads"] + [i.export() for i in self.lines])
 
-    def to_tsv(self):
+    def to_dataframe(self):
         from io import StringIO
         return Utilities.load_tsv(StringIO(self.export()))
