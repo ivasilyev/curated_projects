691ec6d6c0692098c9bf9856aa48a850fb6a2fdd
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-04-04 23:47:37 +0300

    Added the missing consensus taxonomy file import step

--- meta/scripts/silva/prepare_and_import_silva_to_qiime2.sh
+++ meta/scripts/silva/prepare_and_import_silva_to_qiime2.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # It is recommended to launch this script into a working QIIME2 environment
-# or an instance of the official QIIME2 Docker image, e.g.::
+# or an instance of the official QIIME2 Docker image, e.g.:
 #  export IMG=qiime2/core:latest && \
 #  docker pull ${IMG} && \
 #  docker run --rm --net=host -it ${IMG} bash
@@ -31,12 +31,12 @@ python ${SDIR}convert_rna_to_dna.py \
   -i SILVA_138_SSURef_NR99_tax_silva_trunc.fasta \
   -o SILVA_seqs.fasta
 
-echo Remove taxonomy descriptions from FASTA headers, and convert the sequences from RNA to DNA for both the aligned FASTA
+echo Remove taxonomy descriptions from FASTA headers, and convert the sequences from RNA to DNA for the aligned FASTA
 python ${SDIR}convert_rna_to_dna.py -g \
   -i SILVA_138_SSURef_NR99_tax_silva_full_align_trunc.fasta \
   -o SILVA_align_seqs.fasta
 
-echo Remove sequences that contain 5 or more ambiguous bases and/or homopolymers with 8 or more bases
+echo Remove sequences containing 5 or more ambiguous bases and/or homopolymers with 8 or more bases
 python ${SDIR}remove_seqs_with_homopolymers.py \
   -i SILVA_seqs.fasta \
   -o SILVA_seqs_polyfilt.fasta
@@ -46,6 +46,13 @@ python ${SDIR}filter_seqs_by_length_and_taxonomy.py \
     -i SILVA_seqs_polyfilt.fasta \
     -o SILVA_seqs_polyfilt_lenfilt.fasta \
     -t SILVA_138_Taxonomy.txt
+
+echo Import consensus taxonomy file for the full-length sequence
+qiime tools import \
+  --input-path SILVA_138_Taxonomy.txt  \
+  --input-format HeaderlessTSVTaxonomyFormat \
+  --output-path Silva-v138-full-length-seq-taxonomy.qza \
+  --type 'FeatureData[Taxonomy]'
 
 echo Import FASTA sequences
 qiime tools import \
