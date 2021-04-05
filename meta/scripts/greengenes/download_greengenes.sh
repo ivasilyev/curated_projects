5dc50810622788714f8463ad294ba5e2804d7c5c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-04-05 13:57:23 +0300

    Added Greenenes classifier train

--- meta/scripts/greengenes/download_greengenes.sh
+++ meta/scripts/greengenes/download_greengenes.sh
@@ -2,6 +2,7 @@
 
 echo Download the Greengenes database assets
 
+# The Greengenes DB is not updated since May, 2013
 export RDIR="Greengenes_v.13.5"
 mkdir -p "${RDIR}"
 chmod -R 777 "${RDIR}"
@@ -12,7 +13,8 @@ FEXT=$(basename ${URL})
 echo "Fetch ${FEXT}"
 curl -fsSLO ${URL}
 echo "Unpack ${FEXT}"
-tar -xvzf "${FEXT}"
+tar -xzf "${FEXT}"
+rm -f "${FEXT}"
 
 echo Import taxonomy file for the full-length sequence
 qiime tools import \
@@ -26,5 +28,11 @@ qiime tools import \
   --input-path gg_13_5_otus/rep_set/97_otus.fasta \
   --output-path 97_otus-GG.qza \
   --type 'FeatureData[Sequence]'
+
+echo Train classifiers
+qiime feature-classifier fit-classifier-naive-bayes \
+  --i-reference-taxonomy 97_otu-ref-taxonomy-GG.qza \
+  --i-reference-reads 97_otus-GG.qza \
+  --o-classifier gg_13_5_otus-classifier.qza
 
 echo The Greengenes database assets were imported into QIIME2
