55d42619911598b0d519c2457e841680fa77928c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-04-10 01:16:47 +0300

    Added the pathway describing step

--- ashestopalov/nutrition/obesity_metagenomes/2_run_picrust2.sh
+++ ashestopalov/nutrition/obesity_metagenomes/2_run_picrust2.sh
@@ -12,9 +12,27 @@ export SSRC="blood"
 NPROC="$(grep -c '^processor' /proc/cpuinfo)"
 cd "/data1/bio/projects/ashestopalov/nutrition/obesity_metagenomes/qiime2/${SSRC}"
 
+echo Run the PICRUSt2 pipeline
 mkdir -p "picrust2"
 picrust2_pipeline.py --verbose --hsp_method mp \
   --processes ${NPROC} \
   --study_fasta "closed_reference_97/dna-sequences.fasta" \
   --input "biom/feature-table.biom" \
   --output "picrust2"
+
+cd "picrust2"
+
+echo Add KEGG ENZYME descriptions
+add_descriptions.py -m EC \
+  --input "EC_metagenome_out/pred_metagenome_unstrat.tsv.gz" \
+  --output "EC_metagenome_out/pred_metagenome_unstrat_described.tsv"
+
+echo Add KEGG ORTHOLOGY descriptions
+add_descriptions.py -m KO \
+  --input "KO_metagenome_out/pred_metagenome_unstrat.tsv.gz" \
+  --output "KO_metagenome_out/pred_metagenome_unstrat_described.tsv"
+
+echo Add MetaCyc descriptions
+add_descriptions.py -m METACYC \
+  --input "pathways_out/path_abun_unstrat.tsv.gz" \
+  --output "pathways_out/path_abun_unstrat_described.tsv"
