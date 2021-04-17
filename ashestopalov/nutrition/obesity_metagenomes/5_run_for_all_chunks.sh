c1adb48cb4667a2fc38087de1f4b557df612538d
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-04-17 13:53:28 +0300

    Added the sequential processing launch script

---
+++ ashestopalov/nutrition/obesity_metagenomes/5_run_for_all_chunks.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export ROOT_DIR="/data1/bio/projects/ashestopalov/nutrition/obesity_metagenomes/"
+
+cd "${ROOT_DIR}" || exit 1
+ls "sample_data/chopped" | \
+  perl -nle 'm/((stool|blood)[^\.]+)/; print $1' \
+  > "sample_data/chunks.txt"
+cp "sample_data/chunks.txt" \
+  "sample_data/chunks.txt.bak"
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+  printf '%s\n' "Processing $p"
+  bash 1_deploy_qiime2.sh "$p"
+done < "sample_data/chunks.txt"
