92a18453fd7da8b3ec1d3a2084bfac196e203642
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-04-18 17:22:00 +0300

    Added URLs

--- ashestopalov/nutrition/obesity_metagenomes/ansible/run_in_parallel.sh
+++ ashestopalov/nutrition/obesity_metagenomes/ansible/run_in_parallel.sh
@@ -5,12 +5,12 @@ mkdir -p "${PLAYBOOK_DIR}"
 cd "${PLAYBOOK_DIR}" || exit 1
 
 echo Dry run
-curl -fsSLO ""
-ansible-playbook -i ${AWB_HOSTS} --user ${AWB_UN} dry_run.yml
+curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/ashestopalov/nutrition/obesity_metagenomes/ansible/dry_run.yml"
+ansible-playbook -i "${PLAYBOOK_DIR}" --user ${AWB_UN} dry_run.yml
 
 
 
 echo Main run
 cp -r "${QUEUE_FILE}.bak" "${QUEUE_FILE}"
-curl -fsSLO ""
-ansible-playbook -i ${AWB_HOSTS} --user ${AWB_UN} main_run.yml
+curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/ashestopalov/nutrition/obesity_metagenomes/ansible/main_run.yml"
+ansible-playbook -i "${PLAYBOOK_DIR}" --user ${AWB_UN} main_run.yml
