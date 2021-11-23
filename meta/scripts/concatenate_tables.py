85212825390551f3ff7a1c379f82890e1995d02f
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-11-23 19:46:00 +0300

    Added more options to concatenator

--- meta/scripts/concatenate_tables.py
+++ meta/scripts/concatenate_tables.py
@@ -15,19 +15,28 @@ def parse_args():
     parser.add_argument("-i", "--input", nargs="+",
                         help="Tables to concatenate")
     parser.add_argument("-a", "--axis", default=0, type=int, choices=[0, 1],
-                        help="The axis to concatenate along.")
+                        help="(Optional) The axis to concatenate along")
+    parser.add_argument("--index", default="",
+                        help="(Optional) Index to concatenate with")
+    parser.add_argument("--join", default="outer", choices=["inner", "outer"],
+                        help="(Optional) How to handle indexes on other axis (or axes)")
     parser.add_argument("-o", "--output", required=True,
                         help="Output table")
     _namespace = parser.parse_args()
-    return _namespace.input, _namespace.axis, _namespace.output
+    return _namespace.input, _namespace.axis, _namespace.index, _namespace.join, _namespace.output
 
 
 if __name__ == '__main__':
-    input_tables, axis, output_table = parse_args()
+    input_tables, axis, index, join, output_table = parse_args()
     tables = Utilities.remove_empty_values([i for i in input_tables if Utilities.is_file_valid(i)])
     if len(tables) == 0:
         raise ValueError("No valid tables!")
-    dfs = [Utilities.load_tsv(i) for i in tables]
-    out_df = pd.concat(dfs, axis=axis, join="outer", ignore_index=False, keys=None, levels=None,
+    dataframes = [Utilities.load_tsv(i) for i in tables]
+
+    if len(index) > 0:
+        for dataframe in dataframes:
+            dataframe.set_index(inplace=True)
+
+    out_df = pd.concat(dataframes, axis=axis, join=join, ignore_index=False, keys=None, levels=None,
                        names=None, verify_integrity=False, sort=False, copy=True)
     Utilities.dump_tsv(out_df, output_table)
