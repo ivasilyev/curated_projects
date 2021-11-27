36725f9bcb7319991bd35eb797ed42b5130195ab
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-11-27 18:31:51 +0300

    Changed methods name for better verbosity

--- meta/scripts/reference_data.py
+++ meta/scripts/reference_data.py
@@ -13,7 +13,7 @@ class ReferenceData:
         self.refdata_dict = dict()
         self.sequences = ()
 
-    def load(self, refdata_file):
+    def read(self, refdata_file):
         self.refdata_file = refdata_file
         self.refdata_dict = Utilities.load_dict(self.refdata_file)
         self.sequences = tuple(sorted([i for i in self.refdata_dict.keys if i.startswith("sequence_")]))
@@ -25,17 +25,17 @@ class ReferenceData:
         return self.refdata_dict[self.sequences[number - 1]]
 
     @staticmethod
-    def create(refdata_file: str):
+    def load(refdata_file: str):
         refdata = ReferenceData()
-        refdata.load(refdata_file)
+        refdata.read(refdata_file)
         return refdata
 
     @staticmethod
-    def find_refdata(directory: str):
+    def find_and_load_refdata(directory: str):
         refdata_file = Utilities.find_file_by_tail(directory, "_refdata.json")
         if len(refdata_file) == 0:
             raise ValueError(f"Cannot find a RefData file within the directory '{directory}'")
-        return ReferenceData.create(refdata_file)
+        return ReferenceData.load(refdata_file)
 
     def update_metadata(self, d: dict):
         if "metadata" not in self.refdata_dict.keys():
@@ -72,19 +72,6 @@ class AnnotatorTemplate(ABC):
         return
 
 
-class SequenceRetrieverTemplate(ABC):
-    __metaclass__ = ABCMeta
-
-    def __init__(self):
-        super().__init__()
-
-    def download(self):
-        return
-
-    def retrieve(self):
-        return
-
-
 class ReferenceDescriberTemplate(ABC):
     __metaclass__ = ABCMeta
     NAME = ""
@@ -105,14 +92,23 @@ class ReferenceDescriberTemplate(ABC):
     def to_dict(self):
         return Utilities.object_to_dict(self)
 
-    @staticmethod
-    def get_index_guide(raw_nfasta_file):
-        from meta.scripts.LaunchGuideLiner import LaunchGuideLiner
-        index_directory = os.path.join(os.path.dirname(raw_nfasta_file), "index")
-        LaunchGuideLiner.get_index_guide(
-            index_directory=index_directory,
-            raw_nfasta_file=raw_nfasta_file)
-        return index_directory
-
     def describe(self):
+        return
+
+
+class SequenceRetrieverTemplate(ABC):
+    __metaclass__ = ABCMeta
+    VERSION = ""
+    NUCLEOTIDE_FASTA = ""
+    REFERENCE_DIRECTORY = ""
+    REFERENCE_ANNOTATION = ""
+
+    def __init__(self, describer: ReferenceDescriberTemplate):
+        super().__init__()
+        describer.VERSION = self.VERSION
+
+    def download(self):
+        return
+
+    def retrieve(self):
         return
