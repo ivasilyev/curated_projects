13cb1d2f8a5a8d6bce508ed94337a953e669d2db
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-11-28 20:01:12 +0300

    Added VFDB shell launch script

---
+++ meta/scripts/vfdb/index.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function describe {
+    IMG=ivasilyev/curated_projects:latest && \
+    docker pull ${IMG} && \
+    docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it ${IMG} \
+        bash -c '
+            git pull --quiet;
+            python3 "${HOME}/scripts/curated_projects/meta/scripts/vfdb/reference_describer.py" \
+                --output "/data/reference/VFDB"
+        '
+}
+
+describe
+
+bash "/data/reference/VFDB/vfdb_v2021.09.24/index/index.sh"
+
+describe
