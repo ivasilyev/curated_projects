bfebd5b41ea500cb56c638f37c526caafc90f47c
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-12-01 20:31:19 +0300

    Added sequence I/O methods

--- meta/utils/bio_sequence.py
+++ meta/utils/bio_sequence.py
@@ -1,4 +1,37 @@
 
+def remove_duplicate_sequences(records: list):
+    out = []
+    sequences = []
+    for record in records:
+        if record.seq not in sequences:
+            sequences.append(record.seq)
+            out.append(record)
+    return out
+
+
+def load_sequences(file: str, fmt: str = "fasta"):
+    from Bio import SeqIO
+    from meta.utils.primitive import remove_empty_values
+    if fmt == "fastq_gz":
+        import gzip
+        with gzip.open(file, "rt") as f:
+            records = list(SeqIO.parse(f, "fastq"))
+            f.close()
+    else:
+        with open(file, mode="r", encoding="utf-8") as f:
+            records = list(SeqIO.parse(f, fmt))
+            f.close()
+    out = remove_empty_values(sorted(records, key=lambda x: len(x), reverse=True))
+    return out
+
+
+def dump_sequences(sequences: list, file: str, fmt: str = "fasta"):
+    from Bio import SeqIO
+    with open(file, mode="w", encoding="utf-8") as f:
+        SeqIO.write(sequences, f, fmt)
+        f.close()
+
+
 def get_headers_from_fasta(file: str):
     from re import sub
     from meta.utils.primitive import remove_empty_values
@@ -10,12 +43,3 @@ def get_headers_from_fasta(file: str):
         _f.close()
     return sorted(set(remove_empty_values(out)))
 
-
-def remove_duplicate_sequences(records: list):
-    out = []
-    sequences = []
-    for record in records:
-        if record.seq not in sequences:
-            sequences.append(record.seq)
-            out.append(record)
-    return out
