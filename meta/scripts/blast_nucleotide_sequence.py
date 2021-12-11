cf1360a1dabc9601954d442c5232533efd3cdb89
Ilya Vasilyev <u0412u0418u042e@gmail.com>
2021-12-11 21:14:42 +0300

    Switched BLAST search tool to new `utils`

--- meta/scripts/blast_nucleotide_sequence.py
+++ meta/scripts/blast_nucleotide_sequence.py
@@ -8,8 +8,14 @@ from Bio import SeqIO, Entrez
 from time import perf_counter
 from collections import OrderedDict
 from Bio.Blast import NCBIWWW, NCBIXML
+from meta.utils.queue import attempt_func
 from Bio.GenBank import Record as GBRecord
-from meta.scripts.Utilities import Utilities
+from meta.utils.pandas import concat, dump_tsv
+from meta.utils.io import load_dict, dump_dict, dump_string
+from meta.utils.file_system import is_file_valid, filename_only
+from meta.utils.primitive import remove_empty_values, safe_findall
+from meta.utils.date_time import randomize_sleep, count_elapsed_seconds
+from meta.utils.bio_sequence import load_sequences, randomize_gene_slice
 
 
 E_VALUE_THRESH = 0.04
@@ -46,16 +52,16 @@ def parse_largest_subsequence(fasta_nt_file: str):
     """
     Parses nucleotide FASTA and chunk if it's too large for BLAST query
     """
-    assert Utilities.is_file_valid(fasta_nt_file, report=True)
-    records = Utilities.parse_sequences(fasta_nt_file)
-    return Utilities.randomize_gene_slice(records[0], size=QUERY_SIZE).format("fasta")
+    assert is_file_valid(fasta_nt_file, verbose=True)
+    records = load_sequences(fasta_nt_file, "fasta")
+    return randomize_gene_slice(records[0], size=QUERY_SIZE).format("fasta")
 
 
 def download_nt_blast_report(query: str):
     # The delay to avoid NCBI ban
-    Utilities.randomize_sleep(*SLEEP_INTERVAL)
+    randomize_sleep(*SLEEP_INTERVAL)
     # NCBI query
-    result_handle = Utilities.attempt_func(NCBIWWW.qblast, ("blastn", "nt", query))
+    result_handle = attempt_func(NCBIWWW.qblast, ("blastn", "nt", query))
     return NCBIXML.read(result_handle)
 
 
@@ -69,7 +75,7 @@ def parse_blast_report(blast_record):
                 d = dict(title=t, length=alignment.length, expect=hsp.expect, score=hsp.score,
                          bits=hsp.bits, identities=hsp.identities, positives=hsp.positives,
                          query="\n".join([hsp.query, hsp.match, hsp.sbjct, ""]),
-                         geninfo_id=Utilities.safe_findall("\|* *gi\| *([^|]+) *\|", t))
+                         geninfo_id=safe_findall("\|* *gi\| *([^|]+) *\|", t))
                 high_scoring_pairs[t] = d
     return high_scoring_pairs
 
@@ -77,10 +83,11 @@ def parse_blast_report(blast_record):
 def download_reference_genbank(accession_id: str):
     Entrez.email = "name@domain.com"
     # The delay to avoid NCBI ban
-    Utilities.randomize_sleep(*SLEEP_INTERVAL)
+    randomize_sleep(*SLEEP_INTERVAL)
     # NCBI query
-    handle = Utilities.attempt_func(Entrez.efetch, dict(db="nucleotide", id=accession_id,
-                                                        rettype="gb", retmode="text"))
+    handle = attempt_func(Entrez.efetch, dict(
+        db="nucleotide", id=accession_id, rettype="gb", retmode="text"
+    ))
     return list(SeqIO.parse(handle, "genbank"))[0]
 
 
@@ -94,10 +101,11 @@ def describe_reference_genbank(genbank_record: GBRecord):
         cds_number = len([i for i in genbank_record.features if i.type == "CDS"])
         # Otherwise the reference genbank might not be annotated
     qualifiers_dict = [i.qualifiers for i in genbank_record.features if i.type == "source"][0]
-    organism = Utilities.remove_empty_values(qualifiers_dict.get("organism")[0].split(" "))[:2]
+    organism = remove_empty_values(qualifiers_dict.get("organism")[0].split(" "))[:2]
     strain = " ".join(organism + [qualifiers_dict.get("strain")[0]])
-    taxonomy_id = Utilities.safe_findall("\d+", [i for i in qualifiers_dict.get("db_xref") if
-                                                 i.split(":")[0].strip() == "taxon"][0])
+    taxonomy_id = safe_findall(
+        "\d+", [i for i in qualifiers_dict.get("db_xref") if i.split(":")[0].strip() == "taxon"][0]
+    )
     gc_percentage = round(GC(genbank_record.seq), 2)
     return dict(strain=strain, taxonomy_id=taxonomy_id, genbank_id=genbank_record.id,
                 total_cds=cds_number, reference_bp=len(genbank_record),
@@ -106,22 +114,23 @@ def describe_reference_genbank(genbank_record: GBRecord):
 
 if __name__ == '__main__':
     nt_fasta_file, is_blast_only, is_chromosomes_only, blast_result_number, sequence_directory, output_directory = _parse_args()
-    out_blast_basename = os.path.join(output_directory, Utilities.filename_only(nt_fasta_file))
+    out_blast_basename = os.path.join(output_directory, filename_only(nt_fasta_file))
     blast_result_file = "{}_blast_results.json".format(out_blast_basename)
 
-    if Utilities.is_file_valid(blast_result_file, report=False):
-        print("Load results of the already performed NCBI BLAST query")
-        blast_results = Utilities.load_dict(blast_result_file)
+    if is_file_valid(blast_result_file):
+        print(f"Load results of the already performed NCBI BLAST query: '{blast_result_file}'")
+        blast_results = load_dict(blast_result_file)
     else:
-        print("Parsing largest subsequence")
-        blast_query = parse_largest_subsequence(nt_fasta_file)
-        Utilities.dump_string(blast_query, "{}_blast_query.fna".format(out_blast_basename))
-        print(f"Performing BLAST query from the sequence of length {len(blast_query)}")
+        print(f"Parsing largest subsequence from {nt_fasta_file}")
+        blast_query_string = parse_largest_subsequence(nt_fasta_file)
+        blast_query_file = f"{out_blast_basename}_blast_query.fna"
+        dump_string(blast_query_string, blast_query_file)
+        print(f"Performing BLAST query from the sequence of length {len(blast_query_string)}")
         start = perf_counter()
-        blast_report = download_nt_blast_report(blast_query)
-        print(f"The NCBI query was completed with {Utilities.count_elapsed_seconds(start)}")
+        blast_report = download_nt_blast_report(blast_query_string)
+        print(f"The NCBI query was completed with {count_elapsed_seconds(start)}")
         blast_results = parse_blast_report(blast_report)
-        Utilities.dump_dict(blast_results, blast_result_file, sort_keys=False, indent=4)
+        dump_dict(blast_results, blast_result_file, sort_keys=False, indent=4)
 
     if not is_blast_only:
         if len(sequence_directory) == 0:
@@ -132,13 +141,13 @@ if __name__ == '__main__':
         for blast_result in list(blast_results.values()):
             geninfo_accession = blast_result["geninfo_id"]
             genbank_file = os.path.join(sequence_directory, "{}.gbk".format(geninfo_accession))
-            if Utilities.is_file_valid(genbank_file, report=False):
-                genbank_report = Utilities.parse_sequences(genbank_file, "genbank")
+            if is_file_valid(genbank_file):
+                genbank_report = load_sequences(genbank_file, "genbank")
             else:
                 if is_chromosomes_only and " chromosome" not in blast_result["title"]:
                     continue
                 genbank_report = download_reference_genbank(geninfo_accession)
-                Utilities.dump_string(genbank_report.format("genbank"), genbank_file)
+                dump_string(genbank_report.format("genbank"), genbank_file)
             genbank_description = describe_reference_genbank(genbank_report)
             genbank_description["geninfo_id"] = geninfo_accession
             genbank_description["genbank_file"] = genbank_file
@@ -153,10 +162,13 @@ if __name__ == '__main__':
 
         blast_result_df = pd.DataFrame(blast_results.values())
         genbank_description_df = pd.DataFrame(genbank_descriptions)
-        combined_blast_result_df = pd.concat([i.set_index("geninfo_id") for i in (blast_result_df, genbank_description_df)],
-                                             axis=1, sort=False).drop(["query", "genbank_file"], axis=1).dropna(how="any").rename_axis(index="geninfo_id")
-        Utilities.dump_tsv(combined_blast_result_df.reset_index(), os.path.join(output_directory, "combined_blast_results.tsv"))
+        combined_blast_result_df = concat(
+            [blast_result_df, genbank_description_df], index_name="geninfo_id"
+        ).drop(["query", "genbank_file"], axis=1).dropna(axis=0, how="any")
+        dump_tsv(combined_blast_result_df.reset_index(),
+                 os.path.join(output_directory, "combined_blast_results.tsv"))
 
-        report_dict = dict(input_file=nt_fasta_file, genbank_files=genbank_description_df["genbank_file"].values.tolist())
-        Utilities.dump_dict(report_dict, os.path.join(output_directory, "report.json"))
+        report_dict = dict(input_file=nt_fasta_file,
+                           genbank_files=genbank_description_df["genbank_file"].values.tolist())
+        dump_dict(report_dict, os.path.join(output_directory, "report.json"))
     print("Completed")
