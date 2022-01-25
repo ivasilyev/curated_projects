b4e6d7e65a4dd1b2abfdd9536aac2389d9709268
ivasilyev <u0412u0418u042e@gmail.com>
2022-01-25 12:28:41 +0300

    Added verbose output

--- meta/scripts/count_total_wgs_assembly_statistics.py
+++ meta/scripts/count_total_wgs_assembly_statistics.py
@@ -14,12 +14,12 @@ def parse_args():
     from argparse import ArgumentParser
     parser = ArgumentParser(
         description="Tool to count total WGS assembly statistics".strip(),
-        epilog="Any number of reads files may be supplied, only first will be used for coverage count"
+        epilog="Supported formats: https://biopython.org/wiki/SeqIO"
     )
     parser.add_argument("-r", "--raw_reads", required=True, nargs="+",
-                        help="Raw rads")
+                        help="Raw reads files. Any number of reads files may be supplied, only first will be used for coverage count")
     parser.add_argument("--raw_reads_format", default="fastq_gz",
-                        help="(Optional) Raw rads format")
+                        help="(Optional) Raw reads common format")
 
     parser.add_argument("-a", "--assembly", required=True,
                         help="Genome assembly from these reads")
@@ -52,13 +52,16 @@ if __name__ == '__main__':
      reference_format,
      output_file) = parse_args()
 
+    print(f"Counting statistics for {len(raw_reads)} raw reads files")
     raw_stats = {
         os.path.basename(i): Utilities.count_reads_statistics(i, raw_reads_format)
         for i in raw_reads
     }
 
+    print(f"Counting statistics for WGS assembly")
     assembly_stats = Utilities.count_assembly_statistics(assembly_file, assembly_format)
 
+    print(f"Counting statistics for reference")
     reference_sequences = load_sequences(reference_file, reference_format)
     total_reference_sequence = join_sequences(reference_sequences)
     reference_stats = dict(
@@ -66,6 +69,7 @@ if __name__ == '__main__':
         reference_gc_percentage=GC(total_reference_sequence),
     )
 
+    print(f"Counting coverage statistics")
     coverage_stats = Utilities.count_assembly_coverages(
         raw_reads_length_sum=get_first_dict_value(raw_stats)["total_reads_bp"],
         assembly_length=assembly_stats["total_contigs_bp"],
@@ -80,3 +84,4 @@ if __name__ == '__main__':
     )
 
     dump_dict(output_dict, output_file)
+    print(f"Completed")
