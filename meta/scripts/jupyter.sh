dea9cde878460c516a49c53d55933b8c789e6f57
ivasilyev <u0412u0418u042e@gmail.com>
2022-03-03 05:44:49 +0300

    Modified start line for Jupyter server

--- meta/scripts/jupyter.sh
+++ meta/scripts/jupyter.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-export IMG=ivasilyev/curated_projects:latest && \
-docker pull ${IMG} && \
-docker run --rm -v /data:/data -v /data1:/data1 -v /data2:/data2 --net=host -it ${IMG} bash
+export IMG="ivasilyev/curated_projects:latest" && \
+docker pull "${IMG}" && \
+docker run \
+    --interactive \
+    --net=host \
+    --rm \
+    --tty \
+    --volume /data:/data \
+    --volume /data1:/data1 \
+    --volume /data2:/data2 \
+    --volume /data03:/data03 \
+    --volume /data04:/data04 \
+    "${IMG}" bash
 
-git pull && jupyter lab --ip=0.0.0.0 --port=31522 --no-browser --NotebookApp.token=TOKEN
+git pull --quiet && \
+jupyter lab --ip=0.0.0.0 --port=31522 --no-browser --NotebookApp.token=TOKEN
 
 # Web access: `http://<ip>:31522/?token=TOKEN`
