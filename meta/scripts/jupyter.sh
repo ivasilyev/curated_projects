dff4744db71a999182b9432085379dc0e310c174
ivasilyev <u0412u0418u042e@gmail.com>
2022-03-24 07:52:36 +0300

    Added forcing functions to Jupyter deploy script

--- meta/scripts/jupyter.sh
+++ meta/scripts/jupyter.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/env bash
 
+force_docker_pull () {
+    while true
+    do
+        if docker pull "${1}"
+        then
+            return
+        fi
+    done
+}
+
+
+force_git_pull () {
+    while true
+    do
+        if git pull --quiet
+        then
+            return
+        fi
+    done
+}
+
+
 export IMG="ivasilyev/curated_projects:latest" && \
-docker pull "${IMG}" && \
+force_docker_pull "${IMG}" && \
 docker run \
+    --env force_git_pull=force_git_pull \
     --interactive \
     --net=host \
     --rm \
@@ -14,7 +37,12 @@ docker run \
     --volume /data04:/data04 \
     "${IMG}" bash
 
+
 git pull --quiet && \
-jupyter lab --ip=0.0.0.0 --port=31522 --no-browser --NotebookApp.token=TOKEN
+jupyter lab \
+    --ip=0.0.0.0 \
+    --no-browser \
+    --NotebookApp.token=TOKEN \
+    --port=31522
 
 # Web access: `http://<ip>:31522/?token=TOKEN`
