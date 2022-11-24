7c32fdeb07fc90773bc4be9e70b27c31d5fc453c
ivasilyev <u0412u0418u042e@gmail.com>
2022-11-24 17:47:58 +0300

    Modified export-import parts

--- dkhusnutdinova/lactobacteria_wgs_1/0_export_variables.sh
+++ dkhusnutdinova/lactobacteria_wgs_1/0_export_variables.sh
@@ -3,10 +3,10 @@
 export ROOT_DIR="/data03/bio/projects/dkhusnutdinova/lactobacteria_wgs_1/"
 export CARD_VERSION="v3.2.5"
 declare -ax SRC_DIRS=(
-  "/data2/bio/Lactobacillus_Dilyara/raw/"
-  "/data1/bio/220225_NB501097_0075_AHWH5YBGXF/Conversion_lactobacilli/Lactobacilli/"
-  "/data1/bio/220514_M01969_0098_000000000-JWH22/Conversion/lacto/"
-  "/data1/bio/221025_M01969_0103_000000000-KFGYJ/Conversion1/Dilyara_lacto"
+    "/data2/bio/Lactobacillus_Dilyara/raw/"
+    "/data1/bio/220225_NB501097_0075_AHWH5YBGXF/Conversion_lactobacilli/Lactobacilli/"
+    "/data1/bio/220514_M01969_0098_000000000-JWH22/Conversion/lacto/"
+    "/data1/bio/221025_M01969_0103_000000000-KFGYJ/Conversion1/Dilyara_lacto"
 )
 
 mkdir -p "${ROOT_DIR}"
@@ -16,8 +16,3 @@ cd "${ROOT_DIR}" || exit 1
 curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/dkhusnutdinova/lactobacteria_wgs_1/1_run_pipeline.sh"
 bash "1_run_pipeline.sh"
 rm -f "1_run_pipeline.sh"
-
-curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/mshvydkaya/clostridia_wgs/2_combine_coverage_excel.sh"
-sed -E -i "s|--card_version \"[^"]+\"|--card_version "${CARD_VERSION}" \ |g' "2_combine_coverage_excel.sh"
-bash "2_combine_coverage_excel.sh"
-rm -f "2_combine_coverage_excel.sh"
--- dkhusnutdinova/lactobacteria_wgs_1/1_run_pipeline.sh
+++ dkhusnutdinova/lactobacteria_wgs_1/1_run_pipeline.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-# SRC_DIRS and ROOT_DIR are exported from '0_export_variables.sh'
+# CARD_VERSION, SRC_DIRS and ROOT_DIR are exported from '0_export_variables.sh'
+source "0_export_variables.sh"
+
 export ROOT_DIR="$(realpath -s "${ROOT_DIR}")/"
 
 export RAW_DIR="${ROOT_DIR}raw/"
 export SAMPLEDATA_FILE="${ROOT_DIR}sampledata.json"
+export PIPELINE_DIR="${ROOT_DIR}pga-pe-pipeline/"
 
 
 mkdir -p "${RAW_DIR}"
@@ -14,7 +17,6 @@ cd "${ROOT_DIR}" || exit 1
 for SRC_DIR in ${SRC_DIRS[@]}
     do
         export SRC_DIR="$(realpath -s "${SRC_DIR}")/"
-
         echo "Unpack reads from ${SRC_DIR}"
 
         cd "${RAW_DIR}" || exit 1
@@ -88,15 +90,46 @@ curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/biopipelines-docker/mas
 python3 "pipeline_handler.py" \
     --blast_dir "/data/reference/GenBank" \
     --blast_number 25 \
-    --card_json "/data/reference/CARD/card_v3.2.5/card.json" \
+    --card_json "/data/reference/CARD/card_${CARD_VERSION}/card.json" \
     --hg_dir "/data/reference/homo_sapiens/Ensembl/GRCh38/bowtie2_idx" \
     --input "${SAMPLEDATA_FILE}" \
-    --output_dir "${ROOT_DIR}pga-pe-pipeline" \
+    --output_dir "${PIPELINE_DIR}" \
     --refdata \
-        "/data/reference/CARD/card_v3.2.5/index/card_v3.2.5_refdata.json" \
+        "/data/reference/CARD/card_${CARD_VERSION}/index/card_${CARD_VERSION}_refdata.json" \
         "/data/reference/MvirDB/mvirdb_v2012.04.28/index/mvirdb_v2012.04.28_refdata.json" \
         "/data/reference/TADB/tadb_v2017.06/index/tadb_v2017.06_refdata.json" \
         "/data/reference/VFDB/vfdb_v2022.01.21/index/vfdb_v2022.01.21_refdata.json" \
     --srst2_dir "/data/reference/SRST2"
 
 rm -f "pipeline_handler.py"
+
+
+
+echo "Concatenate coverages"
+
+cd "${PIPELINE_DIR}" || exit 1
+
+export IMG=ivasilyev/curated_projects:latest && \
+docker pull "${IMG}" && \/
+docker run \
+    --env PIPELINE_DIR="${PIPELINE_DIR}" \
+    --env CARD_VERSION="${CARD_VERSION}" \
+    --interactive \
+    --net=host \
+    --rm \
+    --volume "/data:/data" \
+    --volume "/data1:/data1" \
+    --volume "/data2:/data2" \
+    --volume "/data03:/data03" \
+    --volume "/data04:/data04" \
+    --tty "${IMG}" \
+        bash -c '
+            git pull --quiet;
+            RGI_DIR="${PIPELINE_DIR}12_rgi/";
+            NBEE_DIR="${PIPELINE_DIR}17_nbee_with_annotation/";
+            python3 ./mshvydkaya/clostridia_wgs/1_concat_coverage_excel.py \
+                --rgi_dir "${RGI_DIR}" \
+                --card_version "${CARD_VERSION}" \
+                --nbee_dir "${NBEE_DIR}" \
+                --output_file "${NBEE_DIR}coverages.xlsx"
+        '
