60f68f7841839dd6b21151aad4096ff71bf1fa7a
ivasilyev <u0412u0418u042e@gmail.com>
2022-11-24 22:01:30 +0300

    Added SE to PE converter script

--- dkhusnutdinova/lactobacteria_wgs_1/1_run_pipeline.sh
+++ dkhusnutdinova/lactobacteria_wgs_1/1_run_pipeline.sh
@@ -117,12 +117,13 @@ docker run \
     --interactive \
     --net=host \
     --rm \
+    --tty \
     --volume "/data:/data" \
     --volume "/data1:/data1" \
     --volume "/data2:/data2" \
     --volume "/data03:/data03" \
     --volume "/data04:/data04" \
-    --tty "${IMG}" \
+    "${IMG}" \
         bash -c '
             git pull --quiet;
             RGI_DIR="${PIPELINE_DIR}12_rgi/";
---
+++ meta/scripts/split_se_to_pe_fastq.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Based on: https://www.biostars.org/p/19446/
+
+import os
+import re
+import joblib as jb
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from meta.utils.bio_sequence import load_sequences, dump_sequences
+
+
+def process_record(record):
+    seq = str(record.seq)
+    rd1_seq = seq[:len(seq)/2]
+    rd2_seq = seq[len(seq)/2:]
+    Q = record.letter_annotations["phred_quality"]
+    rd1_q = Q[:len(Q)/2]
+    rd2_q = Q[len(Q)/2:]
+    rd1_id = record.id.strip("/1").strip("/2") + "/1"
+    rd2_id = record.id.strip("/1").strip("/2") + "/2"
+
+    rd1 = SeqRecord(rd1_seq , id=rd1_id, description="")
+    rd1.letter_annotations["phred_quality"] = rd1_q
+    rd2 = SeqRecord(rd2_seq , id=rd2_id, description="")
+    rd2.letter_annotations["phred_quality"] = rd2_q
+
+    return dict(R1=rd1.format("fastq"), R2=rd2.format("fastq"))
+
+
+def _parse_args():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="")
+    parser.add_argument("-i", "--input_file", metavar="<file>", required=True,
+                        help="Input file")
+    parser.add_argument("-f", "--format", metavar="<str>", default="fastq_gz",
+                        help="Sequence format")
+    parser.add_argument("-o", "--output_dir", metavar="<dir>", required=True,
+                        help="Output directory")
+    _namespace = parser.parse_args()
+    return (
+        _namespace.input_file,
+        _namespace.format,
+        _namespace.output_dir,
+    )
+
+
+if __name__ == '__main__':
+    (
+        input_file,
+        input_format,
+        output_dir,
+    ) = _parse_args()
+
+    output_format = "fastq"
+
+    records = load_sequences(input_file, fmt=input_format)
+    print(f"Parsed {len(records)} sequences from '{input_file}'")
+
+    file_mask = re.sub("(\.gz$|\.fastq\.gz$|\.fastq$|\.fq\.gz$|\.fq$)", "", os.path.basename(input_file))
+    output_files = {f"R{i}": os.path.join(output_dir, f"{file_mask}_R{i}.{output_format}") for i in [1, 2]}
+    print(f"Split SE to PE: '{input_file}' -> '{output_files}'")
+
+    processed_record_dicts = jb.Parallel(n_jobs=-1)(jb.delayed(process_record)(i) for i in records)
+
+    for strand, output_file in output_files.values():
+        processed_records = [i[strand] for i in processed_record_dicts]
+        dump_sequences(processed_records, output_file, fmt=output_format)
+
+    print("Done")
