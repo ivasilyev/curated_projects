827c022deff21260d0e47701b7d1f0e38665dc66
ivasilyev <u0412u0418u042e@gmail.com>
2024-03-21 22:55:25 +0300

    Added QIIME2 reference fetch & import preparation script

---
+++ meta/scripts/silva/0_export_varaiables.sh
@@ -0,0 +1,11 @@
+
+export REFERENCE_ROOT_DIR="/data/reference/SILVA/"
+export SCRIPT_FILE="/tmp/1_fetch_and_import.sh"
+
+
+curl -fsSL "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/meta/scripts/silva/1_fetch_and_import.sh" \
+    -o "${SCRIPT_FILE}"
+
+
+REFERENCE_ROOT_DIR="${REFERENCE_ROOT_DIR}" \
+bash "${SCRIPT_FILE}"
