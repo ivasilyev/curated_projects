cea1bd5b40a6ac00e9381195e867008dd0eb375e
ivasilyev <u0412u0418u042e@gmail.com>
2024-04-20 16:25:09 +0300

    Parametrized group-based scheduler

--- ashestopalov/nutrition/stool_to_blood_2/2_bind_running_commands_by_group.sh
+++ ashestopalov/nutrition/stool_to_blood_2/2_bind_running_commands_by_group.sh
@@ -1,27 +1,30 @@
+#!/usr/bin/env bash
 
-# sudo rm -rf "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/qiime2-picrust2-pipeline"
+export ROOT_DIR="/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/"
+export RAW_DIR="/data03/bio/rogachev_human/"
+export SCRIPT_DIR="${ROOT_DIR}qiime2-picrust2-pipeline/scripts/"
+export SPLIT_DIR="${ROOT_DIR}qiime2-picrust2-pipeline/sample_data/split/"
 
-mkdir -p -m 0777 "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/qiime2-picrust2-pipeline/scripts/"
+# sudo rm -rf "${ROOT_DIR}/qiime2-picrust2-pipeline"
 
-cd "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/qiime2-picrust2-pipeline/scripts/"
+mkdir -p -m 0777 "${SCRIPT_DIR}"
+
+cd "${SCRIPT_DIR}"
 
 curl -fsSLO "https://raw.githubusercontent.com/ivasilyev/curated_projects/master/ashestopalov/nutrition/stool_to_blood_2/3_export_variables.sh"
 
-for GROUP_NAME in \
-    "Non_obese_adult" \
-    "Non_obese_children" \
-    "Sport" \
-    "NUC_children" \
-    "Control" \
-    "Obese_adult" \
-    "NUC_adult" \
-    "Obese_children"
+while read GROUP_NAME
     do
-    echo "Analyze '${GROUP_NAME}'"
+
+    echo "${GROUP_NAME}"
+
+    echo "Run Q2P2 pipeline for '${GROUP_NAME}'"
+
     bash \
-        "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/qiime2-picrust2-pipeline/scripts/3_export_variables.sh" \
+        "${SCRIPT_DIR}3_export_variables.sh" \
         "${GROUP_NAME}" \
-        "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/" \
-        "/data03/bio/rogachev_human/" \
-        "/data03/bio/projects/ashestopalov/nutrition/stool_to_blood_2/qiime2-picrust2-pipeline/sample_data/split/"
-    done
+        "${ROOT_DIR}" \
+        "${RAW_DIR}" \
+        "${SPLIT_DIR}"
+
+    done < "${SPLIT_DIR}sample_groups.txt"
