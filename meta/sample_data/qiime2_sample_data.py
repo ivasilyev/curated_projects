d0950e8d81a259a69b834c2e947a4b381dbb7397
ivasilyev <u0412u0418u042e@gmail.com>
2024-04-28 13:37:26 +0300

    Moved special methods into the dedicated class

--- meta/sample_data/qiime2_sample_data.py
+++ meta/sample_data/qiime2_sample_data.py
@@ -1,74 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import pandas as pd
 from argparse import ArgumentParser
 from meta.sample_data.sample_data import create_sampledata_dict_from_dir, DEFAULT_READS_EXTENSION
-
-
-_Q2_CAT_TYPE = "categorical"
-
-
-def convert_sampledata(
-    sample_data_dict: dict,
-    barcode_sequence: str = "",
-    linker_primer_sequence: str = "",
-):
-    sample_data_dicts = []
-    for sampledata_line in sample_data_dict.values():
-        for sampledata_reads_file, direction in zip(
-            sorted(sampledata_line["reads"]), ["forward", "reverse"]
-        ):
-            sample_data_dicts.append({
-                "sample-id": sampledata_line["name"],
-                "absolute-filepath": sampledata_reads_file,
-                "direction": direction
-            })
-    meta_data_dicts = [{
-        "#SampleID": "#q2:types",
-        "BarcodeSequence": _Q2_CAT_TYPE,
-        "LinkerPrimerSequence": _Q2_CAT_TYPE,
-        "Description": _Q2_CAT_TYPE,
-        "SampleSource": _Q2_CAT_TYPE,
-        "SubjectID": _Q2_CAT_TYPE
-    }, ]
-    for sample_name in sorted(sample_data_dict.keys()):
-        meta_data_dicts.extend([{
-            "#SampleID": sample_name,
-            "BarcodeSequence": barcode_sequence,
-            "LinkerPrimerSequence": linker_primer_sequence,
-            "Description": sample_name,
-            "SampleSource": "".join([i for i in sample_name if i.isalpha()]),
-            "SubjectID": "".join([i for i in sample_name if not i.isalpha()])
-        }])
-    return {
-        "sample": pd.DataFrame(sample_data_dicts).sort_values("absolute-filepath"),
-        "meta": pd.DataFrame(meta_data_dicts)
-    }
-
-
-def convert_and_dump_sampledata(directory: str, *args, **kwargs):
-    dfs = convert_sampledata(*args, **kwargs)
-    os.makedirs(directory, exist_ok=True)
-    out = dict()
-    for key, df in dfs.items():
-        if key == "sample":
-            sep = ","
-            ext = "csv"
-        else:
-            sep = "\t"
-            ext = "tsv"
-        file = os.path.join(directory, f"qiime2_{key}_data.{ext}")
-        df.to_csv(
-            file,
-            sep=sep,
-            header=True,
-            index=False
-        )
-        out[key] = file
-    print(f"Sampledata created in: '{directory}'")
-    return out
+from meta.utils.qiime import convert_and_dump_sampledata
 
 
 def run(
--- meta/utils/qiime.py
+++ meta/utils/qiime.py
@@ -2,6 +2,9 @@
 import pandas as pd
 
 
+_Q2_CAT_TYPE = "categorical"
+
+
 def fix_sample_ids(df: pd.DataFrame):
     # Deblur cannot operate on sample IDs that contain underscores
     df["#SampleID"].replace(
@@ -10,3 +13,65 @@ def fix_sample_ids(df: pd.DataFrame):
         inplace=True,
         regex=True,
     )
+
+
+def convert_sampledata(
+    sample_data_dict: dict,
+    barcode_sequence: str = "",
+    linker_primer_sequence: str = "",
+):
+    sample_data_dicts = []
+    for sampledata_line in sample_data_dict.values():
+        for sampledata_reads_file, direction in zip(
+            sorted(sampledata_line["reads"]), ["forward", "reverse"]
+        ):
+            sample_data_dicts.append({
+                "sample-id": sampledata_line["name"],
+                "absolute-filepath": sampledata_reads_file,
+                "direction": direction
+            })
+    meta_data_dicts = [{
+        "#SampleID": "#q2:types",
+        "BarcodeSequence": _Q2_CAT_TYPE,
+        "LinkerPrimerSequence": _Q2_CAT_TYPE,
+        "Description": _Q2_CAT_TYPE,
+        "SampleSource": _Q2_CAT_TYPE,
+        "SubjectID": _Q2_CAT_TYPE
+    }, ]
+    for sample_name in sorted(sample_data_dict.keys()):
+        meta_data_dicts.extend([{
+            "#SampleID": sample_name,
+            "BarcodeSequence": barcode_sequence,
+            "LinkerPrimerSequence": linker_primer_sequence,
+            "Description": sample_name,
+            "SampleSource": "".join([i for i in sample_name if i.isalpha()]),
+            "SubjectID": "".join([i for i in sample_name if not i.isalpha()])
+        }])
+    return {
+        "sample": pd.DataFrame(sample_data_dicts).sort_values("absolute-filepath"),
+        "meta": pd.DataFrame(meta_data_dicts)
+    }
+
+
+def convert_and_dump_sampledata(directory: str, *args, **kwargs):
+    import os
+    dfs = convert_sampledata(*args, **kwargs)
+    os.makedirs(directory, exist_ok=True)
+    out = dict()
+    for key, df in dfs.items():
+        if key == "sample":
+            sep = ","
+            ext = "csv"
+        else:
+            sep = "\t"
+            ext = "tsv"
+        file = os.path.join(directory, f"qiime2_{key}_data.{ext}")
+        df.to_csv(
+            file,
+            sep=sep,
+            header=True,
+            index=False
+        )
+        out[key] = file
+    print(f"Sampledata created in: '{directory}'")
+    return out
