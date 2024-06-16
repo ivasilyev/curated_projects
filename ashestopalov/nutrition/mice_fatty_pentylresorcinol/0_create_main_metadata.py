8411103c51d32a59048355525636aad2f0a57d55
ivasilyev <u0412u0418u042e@gmail.com>
2024-06-16 21:11:44 +0300

    Added negative control to main metadata

--- ashestopalov/nutrition/mice_fatty_pentylresorcinol/0_create_main_metadata.py
+++ ashestopalov/nutrition/mice_fatty_pentylresorcinol/0_create_main_metadata.py
@@ -2,10 +2,16 @@
 
 import os
 from numpy import nan
-from re import sub
-from meta.utils.qiime import create_main_metadata_df, dump_sampledata, split_main_metadata_df
-from meta.utils.pandas import dfs_dict_to_excel, dump_tsv, excel_to_dfs_dict, split_df
+from re import findall, sub
+from meta.utils.file_system import find_by_regex, symlink
 from meta.utils.language import translate_words
+from meta.utils.pandas import dfs_dict_to_excel, dump_tsv, excel_to_dfs_dict, split_df
+from meta.utils.qiime import (
+    create_main_metadata_df,
+    dump_sampledata,
+    SAMPLE_ID_NAME,
+    split_main_metadata_df,
+)
 
 #%%
 
@@ -57,9 +63,32 @@ group_ids
 
 #%%
 
+source_raw_reads_dir = "/data03/bio/rogachev_human/blood/"
+target_raw_reads_dir = "/data03/bio/rogachev_mice/raw/"
+
+control_regex = "K[I]+"
+control_reads = find_by_regex(f".+{control_regex}.+\.fastq\.gz$", source_raw_reads_dir)
+symlinking_dicts = [
+    {
+        "source": i,
+        "destination": os.path.join(target_raw_reads_dir, os.path.basename(i))
+    }
+    for i in control_reads
+]
+
+symlinking_dicts
+
+#%%
+
+_ = [symlink(**i) for i in symlinking_dicts]
+control_symlinks = [i["destination"] for i in symlinking_dicts]
+control_symlinks
+
+#%%
+
 main_metadata_df = create_main_metadata_df(
     #directory="/data03/bio/rogachev_mice/raw_colon_and_small/",
-    directory="/data03/bio/rogachev_mice/raw/",
+    directory=target_raw_reads_dir,
     barcode_sequence="CTCTCTACTATCCTCT",
     linker_primer_sequence="TCGTCGGCAGCGTCAGATGTGTATAAGAGACAGCCTACGGGNGGCWGCAG",
     sample_source_extraction_function=lambda x: "stool",
@@ -70,14 +99,15 @@ main_metadata_df = create_main_metadata_df(
 )
 
 qiime_types, metadata_df = split_df(main_metadata_df, 1, 0)
-
 excel_dict = {"all_files": metadata_df}
-
 metadata_df
 
 #%%
 
 def validate_subject_id(subject_id: str):
+    # Controls ex.: KIb, KIIb
+    if len(findall(control_regex, subject_id)) > 0:
+        return subject_id
     # Ex.: 066-050-14-10
     if subject_id.count("-") != 3:
         return nan
@@ -87,11 +117,10 @@ def validate_subject_id(subject_id: str):
         return nan
     return subject_id
 
-
 metadata_df["ValidSubjectID"] = metadata_df.loc[:, "SubjectID"].map(validate_subject_id)
 metadata_df = metadata_df.dropna(axis=0, how="any").drop(["ValidSubjectID", "Group"], axis=1)
-metadata_df["FeedDurationsDays"] = metadata_df.loc[:, "SubjectID"].str.extract("\-([^\-]+)\-[^\-]+$").loc[:, 0]
-metadata_df
+feed_duration_column_name = "FeedDurationsDays"
+metadata_df[feed_duration_column_name] = metadata_df.loc[:, "SubjectID"].str.extract("\-([^\-]+)\-[^\-]+$").loc[:, 0]
 
 #%%
 
@@ -103,21 +132,75 @@ merged_metadata_df = metadata_df.merge(
 ).drop(
     [group_df.index.name,],
     axis=1
-)
-merged_metadata_df["FeedDurationsDays"] = merged_metadata_df.loc[:, "FeedDurationsDays"].astype(int)
+).sort_values(SAMPLE_ID_NAME)
+
 merged_metadata_df
 
 #%%
 
-excel_dict.update({"Samplesheet": merged_metadata_df})
+sample_control_metadata_mapper = merged_metadata_df["SamplePath"].isin(control_symlinks)
+
+control_metadata_df = merged_metadata_df.loc[
+    sample_control_metadata_mapper,
+    :
+]
+sample_metadata_df = merged_metadata_df.loc[
+    ~sample_control_metadata_mapper,
+    :
+]
+
+control_value = "ControlNegative"
+for control_metadata_column_name in [
+    "SampleSource",
+    "FeedRation",
+    "ExperimentCode",
+    "Compound",
+    "Subgroup",
+]:
+   control_metadata_df.loc[:, control_metadata_column_name] = control_value
+
+for control_metadata_column_name in [
+    "Group",
+    feed_duration_column_name
+]:
+   control_metadata_df.loc[:, control_metadata_column_name] = 0
+
+control_metadata_df.loc[:, "SubjectID"] = control_metadata_df.loc[:, SAMPLE_ID_NAME]
+
+control_metadata_df
+
+#%%
+
+import pandas as pd
+
+def set_df_dtypes(df: pd.DataFrame, columns_dtypes: dict):
+    for column_name, column_dtype in columns_dtypes.items():
+        df[column_name] = df.loc[:, column_name].astype(column_dtype)
+
+sample_control_metadata_df = pd.concat(
+    [sample_metadata_df, control_metadata_df,],
+    axis=0,
+    sort=True
+)
+set_df_dtypes(
+    sample_control_metadata_df,
+    {i: int for i in ["Group", feed_duration_column_name]}
+)
+sample_control_metadata_df
+
+#%%
+
+excel_dict.update({"Samplesheet": sample_control_metadata_df})
 sampledata_dir = "/data03/bio/projects/ashestopalov/nutrition/mice_fatty_pentylresorcinol/sample_data/"
 dfs_dict_to_excel(excel_dict, os.path.join(sampledata_dir, "samplesheet.xlsx"))
-dump_tsv(merged_metadata_df, os.path.join(sampledata_dir, "main_sampledata.tsv"))
+dump_tsv(sample_control_metadata_df, os.path.join(sampledata_dir, "main_sampledata.tsv"))
 
 #%%
 
-split_sampledata_dict = split_main_metadata_df(merged_metadata_df)
-
-#%%
-
+split_sampledata_dict = split_main_metadata_df(sample_control_metadata_df)
 dump_sampledata(split_sampledata_dict, sampledata_dir)
+
+#%%
+
+# !head /data03/bio/projects/ashestopalov/nutrition/mice_fatty_pentylresorcinol/sample_data/qiime2_meta_data.tsv
+# !tail /data03/bio/projects/ashestopalov/nutrition/mice_fatty_pentylresorcinol/sample_data/qiime2_meta_data.tsv
