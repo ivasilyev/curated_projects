8f6f2185d6ca08a1b18158885c03a68969ffde2f
ivasilyev <u0412u0418u042e@gmail.com>
2024-08-10 21:04:41 +0300

    Added script for 90-day samples only

---
+++ ashestopalov/nutrition/mice_fatty_pentylresorcinol/6_export_variables_90d.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+export ROOT_DIR="/data03/bio/projects/ashestopalov/nutrition/mice_fatty_pentylresorcinol_90/"
+export RAW_DIR="/data03/bio/rogachev_mice/raw/"
+
+export PIPELINE_DIR="$(realpath "${ROOT_DIR}")/qiime2-picrust2-pipeline/"
+export RAW_DIR="$(realpath "${RAW_DIR}")/"
+export LOG_DIR="${PIPELINE_DIR}logs/"
+export MAIN_SAMPLEDATA_URL="https://gitlab.com/ivasilyev/biological_projects/-/raw/main/ashestopalov/nutrition/mice_fatty_pentylresorcinol/sample_data/main_sampledata.tsv"
+export SAMPLEDATA_DIR="${ROOT_DIR}sample_data/"
+export SCRIPT_DIR="${PIPELINE_DIR}scripts/"
+export PIPELINE_SCRIPT="${SCRIPT_DIR}1_run_pipeline"
+export MAIN_SAMPLEDATA_FILE="${SAMPLEDATA_DIR}main_sampledata_90.tsv"
+
+# sudo rm -rf "${ROOT_DIR}"
+mkdir \
+    --mode 0777 \
+    --parents \
+    --verbose \
+    "${ROOT_DIR}" \
+    "${PIPELINE_DIR}" \
+    "${LOG_DIR}" \
+    "${SCRIPT_DIR}" \
+    "${SAMPLEDATA_DIR}"
+chmod -R a+rw "${ROOT_DIR}"
+
+
+curl -fsSL \
+    "${MAIN_SAMPLEDATA_URL}" \
+| grep \
+    --invert \
+    --perl-regexp \
+     '(\t14\t|\t60\t)' \
+| tee "${MAIN_SAMPLEDATA_FILE}"
+
+
+export IMG="ivasilyev/curated_projects:latest"
+docker pull "${IMG}"
+docker run \
+    --env MAIN_SAMPLEDATA_FILE="${MAIN_SAMPLEDATA_FILE}" \
+    --env SAMPLEDATA_DIR="${SAMPLEDATA_DIR}" \
+    --net host \
+    --rm \
+    --volume /data:/data \
+    --volume /data1:/data1 \
+    --volume /data2:/data2 \
+    --volume /data03:/data03 \
+    --volume /data04:/data04 \
+    "${IMG}" \
+        bash -c '
+            git pull --quiet;
+            python3 ./meta/sample_data/qiime/split_qiime2_main_sample_data.py \
+                --input "${MAIN_SAMPLEDATA_FILE}" \
+                --output "${SAMPLEDATA_DIR}"
+        '
+
+cd "${PIPELINE_DIR}" || exit 1
+
+curl -fsSL "https://raw.githubusercontent.com/ivasilyev/biopipelines-docker/master/qiime2_picrust2/shell/1_run_pipeline.sh" \
+    -o "${PIPELINE_SCRIPT}"
+
+ROOT_DIR="${PIPELINE_DIR}" \
+RAW_DIR="${RAW_DIR}" \
+SAMPLEDATA_DIR="${SAMPLEDATA_DIR}" \
+bash "${PIPELINE_SCRIPT}" \
+|& tee "${LOG_DIR}$(basename "${PIPELINE_SCRIPT}").log"
