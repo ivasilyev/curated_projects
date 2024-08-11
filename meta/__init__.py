a00123cd37aba8bae0442631b77f847f4ffae622
ivasilyev <u0412u0418u042e@gmail.com>
2024-08-11 15:36:50 +0300

    Added initial logging instance

--- meta/__init__.py
+++ meta/__init__.py
@@ -1,1 +1,9 @@
 __name__ = "meta"
+
+
+def get_logger():
+    from logging import basicConfig, getLogger, DEBUG
+    basicConfig(format="%(asctime)s %(message)s")
+    logger = getLogger()
+    logger.setLevel(DEBUG)
+    return logger
