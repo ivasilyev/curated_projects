e3c0847470840c14ce285bb7a3cf756ce82bf5d8
ivasilyev <u0412u0418u042e@gmail.com>
2024-08-12 15:54:55 +0300

    Updated `logger` getting method

--- meta/__init__.py
+++ meta/__init__.py
@@ -1,9 +1,19 @@
 __name__ = "meta"
 
+LOGGING_FORMAT = "%(asctime)s.%(msecs)06d [%(filename)s %(name)s] %(levelname)s %(funcName)s(%(lineno)s) - %(message)s"
+DATE_FORMAT = "%Y.%m.%d %H:%M:%S"
+LOGGING_LEVELS = tuple([i * 10 for i in range(5)])
 
-def get_logger():
-    from logging import basicConfig, getLogger, DEBUG
-    basicConfig(format="%(asctime)s %(message)s")
-    logger = getLogger()
-    logger.setLevel(DEBUG)
+
+def get_logger(name: str, level: int):
+    # _LOG = get_logger()
+    import logging as g
+    from sys import stdout
+    assert level in LOGGING_LEVELS
+    logger = g.getLogger(name)
+    logger.setLevel(level)
+    formatter = g.Formatter()
+    stdout_handler = g.StreamHandler(stdout)
+    stdout_handler.setFormatter(formatter)
+    logger.addHandler(stdout_handler)
     return logger
